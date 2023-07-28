4e439c980527e3b92d0db5a1c08f6ab1bd28d2a6
VM - Fábrica <arthur2006.teixeira@gmail.com>
2023-07-28 17:27:37 -0400

    recados opearations

--- includes/pop-ups/pop_ups_mural_novo_recado/pop_ups_mural_recado.php
+++ includes/pop-ups/pop_ups_mural_novo_recado/pop_ups_mural_recado.php
@@ -1,21 +1,23 @@
+<!-- NOVO MURAL -->
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Pop-up</title>
-  <link rel="stylesheet" href="pop_ups_mural_recado.css">
+  <!-- <link rel="stylesheet" href="pop_ups_mural_recado.css">   -->
 </head>
 <body>
   <main class="main-pop-up-thays">
-    <div class="btn-pop">
+    <!-- <div class="btn-pop">
       <button class="btn-popup-thays" onclick="openPopup_recado()">Abrir Pop-up</button>
-    </div>
+    </div> -->
 
     <section>
       <!-- Pop-up -->
       <div class="overlay_recado" id="overlay_recado">
-        <div class="popup-mural-recado">
+        <form method="POST" class="popup-mural-recado">
           <p>Novo recado:</p>
           <div class="div-pop">
               <textarea  placeholder="Digite seu recado: " name="descricao_sala" id="" cols="70" rows="10" class="text-descricao-pop-up"></textarea>
@@ -24,22 +26,22 @@
           </div>
           <section class="container-pop-up-botaoes">
             <div class="botao-padrao-confirmar">
-                <a href="#"><input type="submit" class="botao-confirmar-submit"  value="CONFIRMAR" onclick="closePopup_recado()"></a>
+              <input name="btn_confirmar_submit" type="submit" class="botao-confirmar-submit"  value="CONFIRMAR" onclick="closePopup_recado()">
             </div>
 
             <div class="botao-padrao-cancelar">
-                <a href="#"><input type="submit" class="botao-cancelar-submit"  value="CANCELAR" onclick="closePopup_recado()"></a>
+                <input type="reset" class="botao-cancelar-submit"  value="CANCELAR" onclick="closePopup_recado()">
             </div>
             
             
 
           </section>
       
-        </div>
+        </form>
       </div>
     </section>
   </main>
-
-  <script src="pop_ups_mural_recado.js"></script>
+<!-- 
+  <script src="pop_ups_mural_recado.js"></script> -->
 </body>
 </html>
