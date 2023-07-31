9ec131786e0acc8631a99bc8fdb13a34d10a994b
WanderTerra <wanderleyterra@gmail.com>
2023-07-31 14:15:10 -0400

    Recebendo as permissões do BD

--- pages/edicao_perfil.php
+++ pages/edicao_perfil.php
@@ -3,6 +3,7 @@ require_once("../app/entity/Perfil.php");
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
+
 }
 
 $dadosPerfil = new Perfil();
@@ -11,7 +12,12 @@ $dados = $dadosPerfil->getDados($id);
 $cadastrar_salas = $dados[$id-1] ['cadastrar_sala'];
 $editar_salas = $dados[$id-1] ['editar_sala'];
 $remover_salas = $dados[$id-1] ['remover_sala'];
-
+$validar_checklist = $dados[$id-1] ['validar_checklist'];
+$inserir_item_checklist = $dados[$id-1] ['inserir_item_checklist'];
+$remover_item_checklist = $dados[$id-1] ['remover_item_checklist'];
+$desbloquear_checklist = $dados[$id-1] ['desbloquear_checklist'];
+$descricao_nao_conformidade = $dados[$id-1] ['descricao_nao_conformidade'];
+$enviar_notificacao = $dados[$id-1] ['enviar_notificacao'];
 
 ?>
 <html lang="pt-br">
@@ -48,25 +54,20 @@ $remover_salas = $dados[$id-1] ['remover_sala'];
                 <label for="name" class="input_label">Nome</label> <!--Alterar para o nome do input-->
             </div>
 
-            <div>
-                <?php echo $id; ?>
-            </div>
-
-
             <div class="permissoes_salas">
                 <label class="titulo_permissoes">Permissões da Sala:</label>
                 <div class="permissoes_salas_tipos">
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Cadastrar Salas</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="cadastrar_sala" <?php echo $cadastrar_salas == 1 ? "checked" : "";?>  >
+                        <input type="checkbox" class="checkbox_permissoes" name="cadastrar_sala" <?php echo $cadastrar_salas == 1 ? "checked" : "";?>  />
                     </div>
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Edição de Salas</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="editar_sala"   <?php echo $editar_salas == 1 ? "checked" : "";?>  >
+                        <input type="checkbox" class="checkbox_permissoes" name="editar_sala"   <?php echo $editar_salas == 1 ? "checked" : "";?>  />
                     </div>
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Remover Salas</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="remover_sala"  <?php echo $remover_salas == 1 ? "checked" : "";?>  >
+                        <input type="checkbox" class="checkbox_permissoes" name="remover_sala"  <?php echo $remover_salas == 1 ? "checked" : "";?> />
                     </div>
                 </div>
             </div>
@@ -75,23 +76,23 @@ $remover_salas = $dados[$id-1] ['remover_sala'];
                 <div class="permissoes_salas_tipos">
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Validar Check-List</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="validar_checklist"  autocomplete="off" />
+                        <input type="checkbox" class="checkbox_permissoes" name="validar_checklist" <?php echo $validar_checklist == 1 ? "checked" : "" ?> />
                     </div>
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Inserir Item</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="inserir_item_checklist"  autocomplete="off" />
+                        <input type="checkbox" class="checkbox_permissoes" name="inserir_item_checklist" <?php echo $inserir_item_checklist ==1 ? "checked" : "" ?> />
                     </div>
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Remover Item</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="remover_item_checklist"  autocomplete="off" />
+                        <input type="checkbox" class="checkbox_permissoes" name="remover_item_checklist" <?php echo $remover_item_checklist == 1 ? "checked" : "" ?> />
                     </div>
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Desbloquear Check-List</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="desbloquear_checklist"  autocomplete="off" />
+                        <input type="checkbox" class="checkbox_permissoes" name="desbloquear_checklist" <?php echo $desbloquear_checklist ==1 ? "checked" : "" ?> />
                     </div>
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Descrição de Não Conformidade</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="descricao_nao_conformidade"  autocomplete="off"/>
+                        <input type="checkbox" class="checkbox_permissoes" name="descricao_nao_conformidade" <?php echo $descricao_nao_conformidade == 1 ? "checked" : "" ?> />
                     </div>
                 </div>
             </div>
@@ -100,7 +101,7 @@ $remover_salas = $dados[$id-1] ['remover_sala'];
                 <div class="permissoes_salas_tipos">
                     <div class="permissoes_salas_itens">
                         <label class="label_permissao">Enviar Notificações</label>
-                        <input type="checkbox" class="checkbox_permissoes" name="enviar_notificacao"  autocomplete="off"/>
+                        <input type="checkbox" class="checkbox_permissoes" name="enviar_notificacao" <?php echo $enviar_notificacao == 1 ? "checked" : "" ?> />
                     </div>
                 </div>
             </div>
