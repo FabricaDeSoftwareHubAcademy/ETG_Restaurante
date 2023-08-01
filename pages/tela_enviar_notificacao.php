4f6d1c95e80b0ea4de337afc2c3fcdf74843cb2a
Guilherme Schuindt <guischuindt4@gmail.com>
2023-08-01 16:41:30 -0400

    a

--- pages/tela_enviar_notificacao.php
+++ pages/tela_enviar_notificacao.php
@@ -25,14 +25,14 @@
             <h1>Enviar Notificação</h1>
         </section>
         
-        <!--Input-->
+        <!--Inputs-->
+
         <section class="centralizar-inputs-tela-enviar-notificacao">
             <div class="input_group field">
                 <input type="input" class="input_field" placeholder="Name" required="">
                 <label for="name" class="input_label">Para:</label> <!--Alterar para o nome do input-->
             </div>
 
-            
             <!--Input Email-->
             <div class="input_e-mail_group field">
                 <input type="email" class="input_e-mail_field" placeholder="Name" required="" autocomplete="on">
@@ -40,9 +40,11 @@
             </div>
         </section>
 
-        <!--Botão Voltar-->
-        <section class="centralizar_botoes_enviar_notificacao">
+        <!--Botões-->
 
+        <section class="centralizar_botoes_enviar_notificacao">
+                
+                <!--Botão Voltar-->
             <div class="botao-padrao-voltar">
                 <a href="#"><input type="submit" class="botao-voltar-submit"  value="VOLTAR"></a>
             </div>
