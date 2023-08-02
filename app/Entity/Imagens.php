5da55373882b695380c2d922a8709bfe5c7f58b2
Luiz Guilherme Chimenes <122306667+ChimenesLuiz@users.noreply.github.com>
2023-08-02 14:43:44 -0400

    a
    a

--- app/Entity/Imagens.php
+++ app/Entity/Imagens.php
@@ -22,7 +22,7 @@ class Imagens
             
             $from = $_FILES['imagem_sala']['tmp_name'];
             $to = '../storage/salas/';
-            echo $from . '<br>' . $to . '<br>' . $novo_nome_arquivo;exit;
+            //echo $from . '<br>' . $to . '<br>' . $novo_nome_arquivo;exit;
             move_uploaded_file($from, $to.$novo_nome_arquivo);//movendo o arquivo para pasta
         }
     }
