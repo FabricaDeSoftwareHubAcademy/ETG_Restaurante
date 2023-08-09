e35c0c3969a570bf20a188f977f619dd96d28edb
vINI <brnight05@gmail.com>
2023-08-09 13:50:15 -0400

    a

---
+++ includes/pop-ups/pop_ups senha_alterada/pop_ups verification_senha_alterada.php
@@ -0,0 +1,13 @@
+    <div class="container-pop-up-senha">
+        
+        <div class="popup-senha" id="popup-up-senha">
+            <img src="../includes/pop-ups/pop_ups senha_alterada/imgs/Check_ring.png" alt="carregando">
+            <p>Senha alterada com sucesso </p>
+            <div class="botao-padrao-ok">
+                <a href="#"><input type="submit" class="botao-ok-submit" onclick="closePopupsenha()" value="OK"></a>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="pop_ups verification_senha_alterada.js"></script>
