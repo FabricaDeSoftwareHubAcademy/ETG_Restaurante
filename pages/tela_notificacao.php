cbbd5fba069a407dca29212f8182d4be447b8627
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2023-08-10 15:22:13 -0400

    dsadada
    sdsadsa

---
+++ pages/tela_notificacao.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de notificação </title>
+    <link rel="stylesheet" href="../assets/css/tela_notificacao.css">
+</head>
+<body>
+<?php include_once("../includes/menu.php");?> 
+
+
+    <main class="area_not">
+
+        <title>Notificações</title>
+
+        <div class="notificacao">
+
+        
+
+        </div>
+
+
+    </main>        
+
+
+
+
+</body>
+</html>
