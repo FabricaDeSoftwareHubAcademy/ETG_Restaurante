f00a2a5887015893fe9adf679dc7827ddc62fe7c
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2023-08-10 15:37:26 -0400

    dsadsadsa
    dsadsadsadsa

--- pages/tela_notificacao.php
+++ pages/tela_notificacao.php
@@ -12,11 +12,26 @@
 
     <main class="area_not">
 
-        <title>Notificações</title>
+        <h1 class="titulo_tela_notificacao">Notificações</h1>
 
-        <div class="notificacao">
+        <div class="notificacao_individual">
 
-        
+            <p class="texto_notificacao">
+
+                aqui ficara todo o texto da notificação etc tec te asdmskdgbsjnfgjkbfdh gbdh gbsjkd fgbbgsdb gsjkdbf sd bfghkid bfgdsn fjkndsjk fbsdk fbsjkd fjksdb fjksbdjk fbsdhf sd fjksdb fsdjk fjk
+
+            </p>
+
+            <div class="area_temp_coracao">
+
+                <div class="tempo">5 Min</div>
+
+
+                <div class="coracao"></div>
+
+            </div>
+
+
 
         </div>
 
