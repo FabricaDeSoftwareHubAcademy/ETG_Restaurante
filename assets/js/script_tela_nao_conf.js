b45a4f9221f537930973a2693e0d3190bfc6e063
Leandro Cavalcante Ribas <leandro007cavalcante@gmail.com>
2023-08-14 17:20:39 -0400

    dasdsa
    dsad

--- assets/js/script_tela_nao_conf.js
+++ assets/js/script_tela_nao_conf.js
@@ -3,9 +3,9 @@ let fileInput = document.getElementById("file-input");
 let imageContainer = document.getElementById("images");
 let numOfFiles = document.getElementById("num-of-files");
 
-function preview(){
-    imageContainer.innerHTML = "";
-    numOfFiles.textContent = `${fileInput.files.length} Arquivos escolhidos`;
+    function preview(){
+        imageContainer.innerHTML = "";
+        numOfFiles.textContent = `${fileInput.files.length} Arquivos escolhidos`;
 
     for(i of fileInput.files){
         let reader = new FileReader();
