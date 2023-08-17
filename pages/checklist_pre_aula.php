a8ca928cd4c10a1e1a1aa674c70dcdc78a705f62
WanderTerra <105078842+WanderTerra@users.noreply.github.com>
2023-08-17 17:22:32 -0400

    Front-end parte 2

--- pages/checklist_pre_aula.php
+++ pages/checklist_pre_aula.php
@@ -20,35 +20,90 @@ require ("../vendor/autoload.php");
 </head>
 
 <body class="container_checklist">
-    <h1 class="titulo_checklist">Checklist Pré-Aula</h1>
-    <h2 class="nome_sala">Cozinha Didática 2</h2>
 
-    <main class="main_checklist">
-        <form action="checklist_pre_aula.php" method="POST">
-            <div class="input_checklist">
-                <label class="label_checklist" for="data">Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?</label>
-                    <div class="row">
-                        <div class="checkmark" onclick="check(false)">
-                            <i id="red" class="bi bi-x"></i>
-                        </div>
 
-                        <div class="checkmark" onclick="check(true)">
-                            <i id="green" class="bi bi-check"></i>
+        <h1 class="titulo_checklist">Checklist Pré-Aula</h1>
+        <h2 class="nome_sala">Cozinha Didática 2</h2>
+
+        <main class="main_checklist">
+            <form action="checklist_pre_aula.php" method="POST">
+                <div class="input_checklist">
+                    <label class="label_checklist" for="data">Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?</label>
+
+                        <div class="row">
+                            <div class="checkmark" onclick="check(false)">
+                                <i id="red" class="bi bi-x"></i>
+                            </div>
+
+                            <div class="checkmark" onclick="check(true)">
+                                <i id="green" class="bi bi-check"></i>
+                            </div>
+
                         </div>
-                    </div>
-            </div>
+                </div>
+               
+                <div class="input_checklist">
+                    <label class="label_checklist" for="data">Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?</label>
+
+                        <div class="row">
+                            <div class="checkmark" onclick="check(false)">
+                                <i id="red" class="bi bi-x"></i>
+                            </div>
+
+                            <div class="checkmark" onclick="check(true)">
+                                <i id="green" class="bi bi-check"></i>
+                            </div>
+
+                        </div>
+                </div>
+                <div class="input_checklist">
+                    <label class="label_checklist" for="data">Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?</label>
+
+                        <div class="row">
+                            <div class="checkmark" onclick="check(false)">
+                                <i id="red" class="bi bi-x"></i>
+                            </div>
+
+                            <div class="checkmark" onclick="check(true)">
+                                <i id="green" class="bi bi-check"></i>
+                            </div>
+
+                        </div>
+                </div>
+              
+                </div>
+                <div class="input_checklist">
+                    <label class="label_checklist" for="data">Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?</label>
+
+                        <div class="row">
+                            <div class="checkmark" onclick="check(false)">
+                                <i id="red" class="bi bi-x"></i>
+                            </div>
+
+                            <div class="checkmark" onclick="check(true)">
+                                <i id="green" class="bi bi-check"></i>
+                            </div>
+
+                        </div>
+                </div>
+                
+
+                
+                
+            </form>
             <div class="botoes">
-                <!--Botão Voltar-->
-                <div class="botao-padrao-voltar">
-                    <a href="gerenc_perfis.php"class="botao-voltar-submit">CONFIRMAR</a>
-                </div>
-                <!--Botão Salvar-->
-                <div class="botao-padrao-voltar">
-                    <a href="#"><input name="botao_salvar" type="submit" class="botao-salvar-submit"  value="CANCELAR" onclick="abrir_modal()"></a>
-                </div>
-            </div>
-        </form>
-    </main>
+
+                    <!--Botão Voltar-->
+                    <div class="botao-padrao-voltar">
+                        <a href="gerenc_perfis.php"class="botao-voltar-submit">CONFIRMAR</a>
+                    </div>
+
+                    <!--Botão Salvar-->
+                    <div class="botao-padrao-voltar">
+                        <a href="#"><input name="botao_salvar" type="submit" class="botao-salvar-submit"  value="CANCELAR" onclick="abrir_modal()"></a>
+                    </div>
+        </main>
+
 </body>
 <script src="../assets/js/checklist.js"></script>
 </html>
