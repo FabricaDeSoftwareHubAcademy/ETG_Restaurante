6d3a673c7145f88e1efefeb9025930a5f9819f9c
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2023-08-17 14:38:14 -0400

    dsada
    dsadsa

--- pages/tela_notificacao.php
+++ pages/tela_notificacao.php
@@ -1,6 +1,94 @@
 <?php
 
-require_once("../includes/menu.php");
+
+require __DIR__."/../vendor/autoload.php";
+use App\Entity\Notificacao;
+
+require_once("../includes/menu.php"); 
+
+
+
+
+
+
+
+$notificacao = new Notificacao ;
+
+$notificacao -> getNotificacao();
+
+
+    $todas_notificacao = '' ; ' <div class="notificacao_individual" onclick="trocar_coracao()">
+
+                                    <p class="texto_notificacao" >
+
+                                        .$row_recados['descricao'].
+
+                                    </p>
+
+                                    <div class="area_temp_coracao">
+
+                                        <div class="tempo">5 Min</div>
+
+
+                                        <div class="coracao"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                        </svg></div>
+
+                                        <div class="coracaov2"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                        </svg> </div>
+
+                                    </div>                             
+                                 </div>'
+
+
+
+
+
+foreach ($notificacao as $contador){
+
+    $todas_notificacao .=  
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
 
 
