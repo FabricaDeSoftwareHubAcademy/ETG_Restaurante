3a932ebc194b04259507251198f0ed65fdde55e7
Luiz Guilherme Chimenes <122306667+ChimenesLuiz@users.noreply.github.com>
2023-09-29 14:13:45 -0400

    a
    a

--- pages/cadastrar_notificacao.php
+++ pages/cadastrar_notificacao.php
@@ -3,18 +3,25 @@ require __DIR__."/../vendor/autoload.php";
 require("../includes/header/header.php");
 
 use app\Entity\Notificacao;
+use app\Entity\Usuario;
 
 
 include_once("../includes/menu.php"); 
 require("../includes/main/main_cadastrar_notificacao.php");
 //REGRAS DE NEGOCIO ABAIXO
 
-
+if ()
+{
+    
+}
 $id_remetente = '1'; //VAI PEGAR DA SESSION O ID DO USUARIO
 $email_destinatario = $_POST['email_destinatario']; //email 
 $texto = $_POST['descricao'];
 
-Notificacao::getDados()
+$usuarios = Usuario::getDados();
+var_dump($usuarios);exit;
+
+//Notificacao::getDados()
 
 
 // Notificacao::cadastrar($id_remetente);
