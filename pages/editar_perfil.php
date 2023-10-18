c128b8ec22643a38aff81eb40c34dcc61b99372f
Luiz Guilherme Chimenes <122306667+ChimenesLuiz@users.noreply.github.com>
2023-10-18 15:00:46 -0400

    dsa
    das

--- pages/editar_perfil.php
+++ pages/editar_perfil.php
@@ -9,14 +9,14 @@ if(isset($_GET['id'])){
     $id = $_GET['id'];
     
 }
-//var_dump($_GET);exit;
 //Se o botao_salvar e o nome do cargo estiverem setados
 if (isset($_POST['botao_salvar'], $_POST['nome_cargo']))
 {
     
+
     /*Chamando o metodo setDados que vai alterar os dados do objeto em questao
     Lembrando que o unico obrigatorio e o nome do cargo*/
-    $objPerfil = new Perfil($_POST['nome_cargo'], 
+    $objeto_perfil = new Perfil($_POST['nome_cargo'], 
     (isset($_POST['cadastrar_sala']) ? 1 : 0),
     (isset($_POST['editar_sala']) ? 1 : 0),
     (isset($_POST['remover_sala']) ? 1 : 0),
@@ -26,18 +26,19 @@ if (isset($_POST['botao_salvar'], $_POST['nome_cargo']))
     (isset($_POST['desbloquear_checklist']) ? 1 : 0),
     (isset($_POST['descricao_nao_conformidade']) ? 1 : 0),
     (isset($_POST['enviar_notificacao']) ? 1 : 0));
+
     
     /*Chamando o metodo cadastrar da classe Perfil, essa funcao primeiramente vai verificar se ja existe
     algum perfil com este nome, se sim vai retornar false, senao true, logo, vai cadastrar no banco.*/
     
-    if($objPerfil -> setDados($id))
+    if ($objeto_perfil -> setDados($id))
     {
         header("Location: listar_perfis.php");
     }
 }
 
-$dadosPerfil = new Perfil();
-$dados = $dadosPerfil->getDadosById($id);
+$objeto_perfil = new Perfil();
+$dados = $objeto_perfil -> getDadosById($id);
 $cadastrar_salas = $dados[0] ['realizar_checklist'];
 $editar_salas = $dados[0] ['cadastrar_checklist'];
 $remover_salas = $dados[0] ['editar_checklist'];
@@ -47,7 +48,7 @@ $remover_item_checklist = $dados[0] ['cadastrar_sala'];
 $desbloquear_checklist = $dados[0] ['editar_sala'];
 $descricao_nao_conformidade = $dados[0] ['remover_sala'];
 $enviar_notificacao = $dados[0] ['enviar_notificacao'];
-
+$objeto_perfil = "";
 include_once("../includes/menu.php"); 
 require("../includes/main/main_editar_perfil.php");
 require("../includes/footer/footer.php");
