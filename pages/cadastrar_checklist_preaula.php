1f44d389333b712a0b05ed32ba0c7cd8d8099d2c
WanderTerra <105078842+WanderTerra@users.noreply.github.com>
2023-10-25 16:54:23 -0400

    sda
    dsa

--- pages/cadastrar_checklist_preaula.php
+++ pages/cadastrar_checklist_preaula.php
@@ -1,14 +1,14 @@
 <?php 
+session_start();    
 require __DIR__."/../vendor/autoload.php";
 require("../includes/header/header.php");
 include_once("../includes/menu.php");
-
 use App\Entity\Checklist;
 
 $obj_checklist = new Checklist();
 //REGRAS DE NEGOCIO ABAIXO
 $id_sala = $_GET["id_sala"]; //do metodo GET	
-$id_usuario = $_SESSION["id_user"]; //do metodo GET
+$id_usuario = $_SESSION['id_user']; //do metodo GET
 $data_fechamento = "1970-01-01 00:00:01";
 
 if (isset($_POST["btn_submit"]))
