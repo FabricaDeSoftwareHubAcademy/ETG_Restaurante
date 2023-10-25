6db2efb40b83bbe431a10745992754447eb52537
Luiz Guilherme Chimenes <122306667+ChimenesLuiz@users.noreply.github.com>
2023-10-25 14:20:33 -0400

    aa

--- pages/listar_recados.php
+++ pages/listar_recados.php
@@ -26,31 +26,26 @@ if(isset($_POST['btn_confirmar_submit'])){
 
 // pegando informações dos recados 
 
-    $recados = Recado::getDados()->fetchAll(PDO::FETCH_ASSOC)  ?  Recado::getDados()->fetchAll(PDO::FETCH_ASSOC) : "";
+$recados = Recado::getDados()->fetchAll(PDO::FETCH_ASSOC)  ?  Recado::getDados()->fetchAll(PDO::FETCH_ASSOC) : "";
 
-    $cards_recados = '';
+$cards_recados = '';
 
-    foreach($recados as $row_recados){
+foreach($recados as $row_recados){
 
-        $cards_recados .= '<div class="card">
-                                <p class="msg_aviso">'.htmlspecialchars($row_recados['texto']).'</p>
-                                <div class="area_bot">
+    $cards_recados .= '<div class="card">
+                            <p class="msg_aviso">'.htmlspecialchars($row_recados['texto']).'</p>
+                            <div class="area_bot">
 
-                                    <div class="bloco_bot">
+                                <div class="bloco_bot">
 
-                                        <img class="icon_card_recado_editar" id_recado="'.$row_recados['id'].'" src="../assets/imgs/icons/btn_editar.png">
-                                        <img class="icon_card_recado_excluir" id_recado="'.$row_recados['id'].'" src="../assets/imgs/icons/icon_trash.png">
+                                    <img class="icon_card_recado_editar" id_recado="'.$row_recados['id'].'" src="../assets/imgs/icons/btn_editar.png">
+                                    <img class="icon_card_recado_excluir" id_recado="'.$row_recados['id'].'" src="../assets/imgs/icons/icon_trash.png">
 
-                                    </div>
                                 </div>
-                            </div>';
-                            
+                            </div>
+                        </div>';
+                        
 
-     }
-     require("../includes/main/main_listar_recados.php");
+    }
+    require("../includes/main/main_listar_recados.php");
 ?>
-
-
- 
-
- 
