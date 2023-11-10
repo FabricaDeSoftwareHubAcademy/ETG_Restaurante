efb03cd87c290f5748623a110829b01f994a6f2a
Igor <igoralbieri04@gmail.com>
2023-11-10 17:24:36 -0400

    pok

--- includes/menu.php
+++ includes/menu.php
@@ -68,6 +68,9 @@
                             <li class="iten-submenu"><a href="../pages/cadastrar_notificacao.php" id="fonte-submenu">Cadastro de Notificações</a></li> 
                             <li class="iten-submenu"><a href="../pages/cadastrar_usuario.php" id="fonte-submenu2">Cadastro de Usuário</a></li> 
                             <li class="iten-submenu"><a href="../pages/cadastrar_perfil.php" id="fonte-submenu2">Cadastro de Perfil</a></li> 
+                            <li class="iten-submenu"><a href="../pages/cadastrar_pergunta.php" id="fonte-submenu">Cadastro de Perguntas</a></li>  
+                            <li class="iten-submenu"><a href="../pages/cadastrar_notificacao.php" id="fonte-submenu">Cadastro de Notificações</a></li>                      
+
                         </ul>
                     </li> 
                 </li>         
