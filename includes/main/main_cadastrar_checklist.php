5cc5e375457cfb9dde78f10483dbd6c2e9c3e5b0
= <=>
2023-11-20 14:43:05 -0400

    asd
    sad

--- includes/main/main_cadastrar_checklist.php
+++ includes/main/main_cadastrar_checklist.php
@@ -1,19 +1,18 @@
+
+<link rel="stylesheet" href="estilo_perfil.css">
 <link rel="stylesheet" href="https/cdnjs.cloudflare.comlibs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
-
-<link rel="stylesheet" href="../assets/css/cadastrar_checklist/input_checklist.css">
+<link rel="stylesheet" href="../assets/css/cadastrar_checklist/input-checklist.css">
 <link rel="stylesheet" href="../assets/css/cadastrar_checklist/posicao.css">
+<link rel="stylesheet" href="../assets/js/cadastro-checklist.js">
 <link rel="stylesheet" href="../assets/css/cadastrar_checklist/caixa_pergunta.css">
-<link rel="stylesheet" href="../assets/css/cadastrar_checklist/botões_checklist.css">
-
-
+<link rel="stylesheet" href="../assets/css/cadastrar_checklist/botões-checklist.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
 
 
 <body class="pai_de_todos">
-
-
+    <?php include_once("../includes/menu.php"); ?>
     <div class="titulo-topo">
         <h1 id="titulo">Cadastro de CheckList</h1>
     </div>
@@ -24,40 +23,63 @@
 
             <div class="inputs-cadastro-checklist">
                 <div class="input_group field">
-                    <input type="input" class="input_field" placeholder="Name" required="" name='nome-checklist'>
-                    <label for="name" class="input_label">Nome do CheckList</label> <!--Alterar para o nome do input-->
+                    <input type="input" class="input_field" placeholder="Name" required="" name="nome-checklist"  maxlength="45">
+                    <label for="name" class="input_label">Nome da CheckList</label> <!--Alterar para o nome do input-->
+                </div>
+            </div>
+
+            <!-- Menu das abas -->
+            <ul class="tab-menu">
+                <li class="tab-button active" onclick="showTab('tab1')">Pré-Aula</li>
+                <li class="tab-button" onclick="showTab('tab2')">Pós-Aula</li>
+                <div class="line"></div>
+            </ul>
+
+            <!-- Conteúdo das abas -->
+            <div id="tab1" class="tab" style="display: block;">
+                <div class="titulo-selecione-pergunta-pre">
+                    <h1 id="titulo-pergunta-pos">Selecione as Perguntas:</h1>
                 </div>
 
-                    <div class="titulo-selecione-pergunta-pre">
-                        <h1 id="titulo-pergunta-pos">Selecione as Perguntas Pré-Aula:</h1>
-                    </div>
-
                 <section class="selecao-pergunta">
                     <table class="tabela-perguntas">
                         <tr class="topo-tabela">
                             <th>Selecione</th>
                             <th>Pergunta</th>
                         </tr>
-                        <?=$tr?>
-
-
-                    </table>
-                </section>
-                <div class="titulo-selecione-pergunta-pos">
-                        <h1 id="titulo-pergunta-pos">Selecione as Perguntas Pós-Aula:</h1>
-                    </div>
-                <section class="selecao-pergunta">
-                    <table class="tabela-perguntas">
-                        <tr class="topo-tabela">
-                            <th>Selecione</th>
-                            <th>Pergunta</th>
-                        </tr>
-                        <?=$tr?>
-
-
+                        <?= $tr ?>
                     </table>
                 </section>
             </div>
+
+            <div id="tab2" class="tab">
+                <div class="titulo-selecione-pergunta-pos">
+                    <h1 id="titulo-pergunta-pos">Selecione as Perguntas:</h1>
+                </div>
+                <section class="selecao-pergunta">
+                    <table class="tabela-perguntas">
+                        <tr class="topo-tabela">
+                            <th>Selecione</th>
+                            <th>Pergunta</th>
+                        </tr>
+                        <?= $tr ?>
+                    </table>
+                </section>
+            </div>
+
+
+            <script>
+                function showTab(tabId) {
+                    const tabs = document.querySelectorAll('.tab');
+                    const buttons = document.querySelectorAll('.tab-button');
+
+                    tabs.forEach(tab => tab.style.display = 'none');
+                    buttons.forEach(button => button.classList.remove('active'));
+
+                    document.getElementById(tabId).style.display = 'block';
+                    document.querySelector(`[onclick="showTab('${tabId}')"`).classList.add('active');
+                }
+            </script>
 
             <div class="botoes">
                 <div class="botoes-cadastro-checklist">
@@ -75,6 +97,55 @@
         </form>
     </main>
 
+
+    <!----------------- Sanduiche------------------>
+    <script>
+        var toggleClick = document.querySelector(".toggleBox-menu");
+        var container = document.querySelector(".container-menu");
+        toggleClick.addEventListener("click", () => {
+            toggleClick.classList.toggle("active");
+            container.classList.toggle("active");
+            submenu.classList.remove('active');
+            btn_submenu.setAttribute('onclick', 'openSubmenu()');
+            closeModal()
+        })
+
+        const modal = document.querySelector('.modal-container-menu')
+        const submenu = document.querySelector('.sub-menu')
+        const btn_submenu = document.getElementById('btnsubmenu')
+
+        function openModal() {
+            modal.classList.add('active')
+        }
+
+        function closeModal() {
+            modal.classList.remove('active')
+        }
+
+        function openSubmenu() {
+            submenu.classList.add('active')
+            btn_submenu.setAttribute('onclick', 'closeSubmenu()')
+        }
+
+        function closeSubmenu() {
+            submenu.classList.remove('active')
+            btn_submenu.setAttribute('onclick', 'openSubmenu()')
+        }
+    </script>
+
+    <!-- pop-up -->
+
+
+    <script>
+        function abrir_modal() {
+            Swal.fire({
+                title: 'Cadastrado com sucesso!', //TITULO DO POP_UP DE ACORDO COM SUA TELA 
+                icon: 'success', // success, error e warning
+                confirmButtonColor: '#609437', // DEFINE A COR DO BOTÃO OK
+                confirmButtonText: 'OK'
+            });
+        }
+    </script>
 
 
 </body>
--- pages/cadastrar_checklist.php
+++ pages/cadastrar_checklist.php
@@ -1,62 +1,50 @@
 <?php
-
-require __DIR__."/../vendor/autoload.php";
-include_once("../includes/menu.php"); 
+require __DIR__ ."/../vendor/autoload.php";
 $titulo_page = 'Cadastrar Checklist';
+include_once("../includes/menu.php"); 
 require("../includes/header/header.php");
 require("../includes/main/main_cadastrar_checklist.php");
-//REGRAS DE NEGOCIO ABAIXO
-
-
-
-//FIM DAS REGRAS DE NEGOCIO
-
 
 use App\Entity\Pergunta;
 use App\Entity\CadastroChecklist;
+
 $dados = Pergunta::getDados()->fetchAll(PDO::FETCH_ASSOC);
 $tr = "";
-foreach ($dados as $rowdados){
+
+foreach ($dados as $rowdados) {
     $tr .= "<tr> 
-                <td><input type='checkbox'  id='checkbox' name='pergunta[]' value='" . $rowdados['id_cadastro_pergunta'] . "'></td>
-                <td>" . $rowdados['descricao'] . "</td>   
+                    <td><input type='checkbox'  id='checkbox' name='pergunta[]' value='" . $rowdados['id_cadastro_pergunta'] . "'></td>
+                    <td>" . $rowdados['descricao'] . "</td>   
                 </tr>";
-                
-                
+}
 
-            }
-            
-            if(isset($_POST['cadastrar'])){
+if (isset($_POST['cadastrar'])) {
 
-                $check = new CadastroChecklist();
-    $check -> nome = $_POST['nome-checklist'];
+    $check = new CadastroChecklist();
+    $check->nome = $_POST['nome-checklist'];
     $id = $check->cadastrar();
 }
 
-if(isset($_POST[''])){
+if (isset($_POST[''])) {
 
     $check = new CadastroChecklist();
     $check   = $_POST['id_cadastro_pergunta'];
     $id = $check->cadastrar();
-
-
 }
-if (isset($_POST['pergunta'])){
+if (isset($_POST['pergunta'])) {
     $perguntas = $_POST['pergunta'];
-    $check -> cadastrarPergunta($perguntas,$id);
-    
+    $check->cadastroPergunta($perguntas, $id);
+
     echo ("<script>
     function abrir_modal(){
         Swal.fire({
             title: 'Cadastrado com sucesso!', //TITULO DO POP_UP DE ACORDO COM SUA TELA 
             icon: 'success', // success, error e warning
-        confirmButtonColor: '#609437', // DEFINE A COR DO BOTÃO OK
-        confirmButtonText: 'OK'
-    });
+            confirmButtonColor: '#609437', // DEFINE A COR DO BOTÃO OK
+            confirmButtonText: 'OK'
+        });
+    }
+        </script>");
 }
-</script>");
 
-}
-
-require("../includes/footer/footer.php");
-?> 
+?>
