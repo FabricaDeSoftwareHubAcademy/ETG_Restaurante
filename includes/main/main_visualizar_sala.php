2fa96f770cb992f47c97d50a364d17851ca70f73
Joceyr Gomes Martins <joeymartins00@gmail.com>
2023-11-28 15:10:45 -0400

    yuy
    uuy

--- includes/main/main_visualizar_sala.php
+++ includes/main/main_visualizar_sala.php
@@ -12,80 +12,84 @@
 
 <body class="visualizar_sala">
     <div class="pagina-visualizar-sala">
+            <div class="nome_da_sala" id="nome_da_sala">
+                <h1>VISUALIZAR andre</h1>
+            </div>
         <section class="area_card">
-            <div class="card">
-                <div class="imagem_card">
-                        
-                    <img src="../storage/salas/<?= $dados[0]['img_sala']?>" alt="" id="img_config">
-                </div>
-                <div class="texto_card">
-
-                    <div class="titulo_card">
-                        <p class="label-sala">Nome da Sala</p>
-                        <h1 class="nome-sala"><?=$dados[0]['nome']?></h1>
+            
+                <div class="card">
+                    <div class="imagem_card">
+                            
+                        <img src="../storage/salas/<?= $dados[0]['img_sala']?>" alt="" id="img_config">
                     </div>
-        
+                    <div class="texto_card" >
 
-                        <div class="paragrafo_card">
-                            <h2 id="paragrafo_descricao">
-                                Descrição
-                            </h2>
-
-                            <p class="texto-descricao"> <?=$dados[0]['descricao']?></p>
-                            
-                            
+                        <div class="titulo_card">
+                            <p class="label-sala">Nome da Sala</p>
+                            <h1 class="nome-sala"><?=$dados[0]['nome']?></h1>
                         </div>
-                        <div class="dias-de-funcionamento">
-                            <h2 id="dias-funcionamento">
-                                Dias de Funcionamento
-                            </h2>
+            
 
-                            <p class="dias_funcionamento"> <?php
-                            
-                                echo $segunda.'  ';
-                                echo $terca.' ';
-                                echo $quarta.' ';
-                                echo $quinta.' ';
-                                echo $sexta.' ';
-                                echo $sabado.' ';
+                            <div class="paragrafo_card">
+                                <h2 id="paragrafo_descricao">
+                                    Descrição
+                                </h2>
+
+                                <p class="texto-descricao"> <?=$dados[0]['descricao']?></p>
+                                
+                                
+                            </div>
+                            <div class="dias-de-funcionamento">
+                                <h2 id="dias-funcionamento">
+                                    Dias de Funcionamento
+                                </h2>
+
+                                <p class="dias_funcionamento"> <?php
+                                
+                                    echo $segunda.'  ';
+                                    echo $terca.' ';
+                                    echo $quarta.' ';
+                                    echo $quinta.' ';
+                                    echo $sexta.' ';
+                                    echo $sabado.' ';
 
 
-                            ?></p>
-                            
-                            
-                        </div>
-                        <div class="turnos-de-funcionamento">
-                            <h2 id="turnos-funcionamento">
-                                Turnos de Funcionamento
-                            </h2>
+                                ?></p>
+                                
+                                
+                            </div>
+                            <div class="turnos-de-funcionamento">
+                                <h2 id="turnos-funcionamento">
+                                    Turnos de Funcionamento
+                                </h2>
 
-                            <p class="turnos_funcionamento"> <?php
-                            
-                                echo $matutino.' ';
-                                echo $vespertino.' ';
-                                echo $noturno.' ';
+                                <p class="turnos_funcionamento"> <?php
+                                
+                                    echo $matutino.' ';
+                                    echo $vespertino.' ';
+                                    echo $noturno.' ';
 
-                            
-                            ?></p>
-                            
-                            
-                        </div>
-                                            
-                </div>
-            </div>
-
-            <div class="alinhar_botoes">
-
-                <!--Botão Voltar-->
-                <div class="botao-padrao-voltar">
-                    <a href="#"><input type="submit" class="botao-voltar-submit"  value="VOLTAR"></a>
-                </div>
-                <!--Botão Checklist -->
-                <div class="botao-padrao-fazer-checklist">
-                    <a href="#"><input type="submit" class="botao-fazer-checklist-submit"  value="FAZER CHECKLIST"></a>
+                                
+                                ?></p>
+                                
+                                
+                            </div>
+                                                
+                    </div>
                 </div>
 
-            </div>
+                <div class="alinhar_botoes">
+
+                    <!--Botão Voltar-->
+                    <div class="botao-padrao-voltar">
+                        <a href="#"><input type="submit" class="botao-voltar-submit"  value="VOLTAR"></a>
+                    </div>
+                    <!--Botão Checklist -->
+                    <div class="botao-padrao-fazer-checklist">
+                        <a href="#"><input type="submit" class="botao-fazer-checklist-submit"  value="FAZER CHECKLIST"></a>
+                    </div>
+
+                </div>
         </section>
     </div>
     
