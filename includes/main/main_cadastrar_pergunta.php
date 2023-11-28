43e9a9a4d14b6340cb3bd5f688c68fd4ba2b89a4
Leandro Cavalcante Ribas <leandro007cavalcante@gmail.com>
2023-11-28 17:17:26 -0400

    a
    a

--- includes/main/main_cadastrar_pergunta.php
+++ includes/main/main_cadastrar_pergunta.php
@@ -6,53 +6,75 @@
 
 
 <body>
+    
     <main class="meio">
-        <h1 id="titulo">
-            Cadastro de Perguntas
-         </h1>
-        <div class="aunt">
-            <div class="pesquisa">
-                <div class="input_group field">
-                    <input type="input" id ="input" class="input_field" placeholder="." required="" name="nome-checklist" autocomplete="off">
-                    <label for="name" class="input_label">Descrição da pergunta
-                    </label> <!--Alterar para o nome do input-->
-                    <i class="bi bi-search" id="lupa"></i>
-                </div>
-            </div>
-            <section class="perguntas">
-                <?php
 
-                echo($divs);
-                ?>
-                
-            </section>
-            
-            <div class="botao">
-                <button  class="botao-cadastrar-submit"  value="CADASTRAR" onclick="openPopup()">CADASTRAR</button>
-            </div>
-            <form>
-                <div class="popup" id="popup">
-                    <h4>Nova pergunta:</h4>
-                    <textarea name="nova_pergunta" class="nova_pergunta" placeholder= "Escreva o conteúdo da pergunta"cols="30" rows="10" autocomplete= "off"></textarea>
-                    <h4>A pergunta será para: </h4>
-                    <div class="checks">
-                        <div class="check1">
-                            <input type="checkbox" name="antes_da_aula" id=""> Antes da aula
-                        </div>
-                        <div class="check2">
-                            <input type="checkbox" name="depois_da_aula" id=""> Depois da aula
-                        </div>
-                    </div>
-                    <div class="botoes">
-                        <button class="botao-cancelar-submit"  value="CANCELAR" onclick="closePopup()">CANCELAR</button>
-                        <button class="botao-confirmar-submit"  value="CONFIRMAR">CONFIRMAR</button>
+        <!-- TÍTULO DA PÁGINA -->
+        <h1 id="titulo">Cadastro de Perguntas</h1>
+        <div class="aunt">
+
+            <!-- PARTE DA PESQUISA  -->
+
+                <div class="pesquisa">
+                    <div class="input_group field">
+                        <input type="input" id ="input" class="input_field" placeholder="." required="" name="nome-checklist" autocomplete="off">
+                        <label for="name" class="input_label">Descrição da pergunta</label> 
+                        <i class="bi bi-search" id="lupa"></i>
                     </div>
                 </div>
-            </form>
+            
+            <!-- AQUI ELE ESTÁ PRINTANDO AS PERGUNTAS DO BANCO NOS CARDS (ver em "cadastrar_pergunta.php")-->
+                
+                <section class="perguntas">
+                    <?php
+
+                    echo($divs);
+                    ?>
+                    
+                </section>
+            
+            <!-- BOTÃO DE CADASTRAR -->
+                <div class="botao">
+                    <button  class="botao-cadastrar-submit"  value="CADASTRAR" onclick="openPopup()">CADASTRAR</button>
+                </div>
         </div>
+
+        <!-- POPUP DE CADASTRAR PERGUNTA -->
+
+        <form>
+            <div class="popup" id="popup">
+                <h4>Nova pergunta:</h4>
+
+                <textarea name="nova_pergunta" class="nova_pergunta" placeholder= "Escreva o conteúdo da pergunta"cols="30" rows="10" autocomplete= "off"></textarea>
+
+                <h4>A pergunta será para: </h4>
+
+                <!-- DIV DAS 2 CHECKBOX'S -->
+                
+                <div class="checks">
+                    <div class="check1">
+                        <input type="checkbox" name="antes_da_aula" id=""> Antes da aula
+                    </div>
+                    <div class="check2">
+                        <input type="checkbox" name="depois_da_aula" id=""> Depois da aula
+                    </div>
+                </div>
+
+                <!-- DIV DOS BOTÕES (cancelar e confirmar) -->
+                <div class="botoes">
+                    <button class="botao-cancelar-submit"  value="CANCELAR" onclick="closePopup()">CANCELAR</button>
+                    <button class="botao-confirmar-submit"  value="CONFIRMAR">CONFIRMAR</button>
+                </div>
+            </div>
+        </form>
+        
         
     </main>
+
+    
 <script>
+
+    // SCRIPT DO POPUP DE CADASTRAR PERGUNTAS
 
     let popup = document.getElementById("popup");
     function openPopup(){
@@ -63,7 +85,9 @@
     }
 
 </script>
+
 </body>
+
 <!-- Script -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
