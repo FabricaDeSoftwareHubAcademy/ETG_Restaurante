0426d49baf4e9abca1ae760a4a5650daf9e7b3d3
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2023-11-29 14:41:40 -0400

    dasdsa
    dsadsa

--- includes/pop-ups/pop_ups cadastrar_sala.php
+++ includes/pop-ups/pop_ups cadastrar_sala.php
@@ -1,19 +1,13 @@
-<!-- <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ETG</title>
-    <link rel="stylesheet" href="pop-up-excluir.css">
-</head>
-<body> -->
+
+<link rel="stylesheet" href="../assets/css/pop_ups/pop-up-cadastrar_sala.css">
+
 
     <div class="container-pop-up-notificacao">
         <button type="submit" class="btn-pop-up-notificacao" id="submit-btn-notificacao" onclick="openPopupValidar()">Submit</button>
         <div class="popup-notificacao" id="popup-up-notificacao">
             <div class="div-img">
-                <!-- <img src="../img/erro.png" alt="carregando" id="img_check"> -->
-                <p>Recado excluído com sucesso! </p>
+                <img src="../img/erro.png" alt="carregando" id="img_check">
+                <p>Sala Cadastrada Com sucesso ! </p>
             </div>
             <div class="botao-padrao-ok">
                 <a href="#"><input type="submit" class="botao-ok-submit" onclick="closePopupValidar()" value="OK"></a>
@@ -22,6 +16,6 @@
     </div>
 
 
-    <!-- <script src="pop-up-excluir.js"></script> -->
+    <script src="pop-up-excluir.js"></script>
 <!-- </body>
 </html> -->
