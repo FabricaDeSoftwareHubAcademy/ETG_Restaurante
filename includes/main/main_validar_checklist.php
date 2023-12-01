b3c1544d9282a78e7c6f4ea850030520f05cd1c6
= <=>
2023-12-01 17:18:28 -0400

    asd
    asd

--- includes/main/main_validar_checklist.php
+++ includes/main/main_validar_checklist.php
@@ -1,90 +1,106 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-<body>
-    <section class="tudo">
-        <div class="cabecalho">
-            <div class="titulo"> 
-                <h1>Validação do Checklist</h1>
-            </div>
-            
-            <div class="gear-second">
-                <div class="item">
-                    <span class="label">Professor:</span>
-                    <span class="value">Cristiane</span>
-                </div>
-                <div class="item">
-                    <span class="label">Ambiente:</span>
-                    <span class="value">Cozinha Didática I</span>
-                </div>
-            </div>
-
-            <div class="gear-third">
-                <div class="item">
-                    <span class="label">Data:</span>
-                    <span class="value">03/03/2023</span>
-                </div>
-
-                <div class="item">
-                    <span class="label">Aberto:</span>
-                    <span class="value">7:30 h</span>
-                </div>
-
-                <div class="item">
-                    <span class="label">Encerrado:</span>
-                    <span class="value">11:50 h</span>
-                </div>
-                </div>
-            
-            </div>
-
-            <div class="inputs">
-                <div class="area-check">
-                    <div class="input">                      
-                        <p class = "texto-checklist">
-                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
-                        </p>
-                    </div>
-                    <i class="bi bi-check-circle" id="check"></i>
-                </div>               
-                <div class="area-check">
-                    <div class="input">                      
-                        <p class = "texto-checklist">
-                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
-                        </p>
-                    </div>
-                    <i class="bi bi-x-circle" id="not-check"></i>
-                </div>
-                <div class="area-check">
-                    <div class="input">                      
-                        <p class = "texto-checklist">
-                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
-                        </p>
-                    </div>
-                    <i class="bi bi-check-circle" id="check"></i>
-                </div>
-                <div class="area-check">
-                    <div class="input">                      
-                        <p class = "texto-checklist">
-                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
-                        </p>
-                    </div>
-                    <i class="bi bi-check-circle" id="check"></i>
-                </div>
-            </div>
-        </div>
-        <div class="alinar-botoes">
-            <div class="botao-padrao-voltar">
-                <a href="listar_salas.php" class="botao-voltar-link">VOLTAR</a>
-            </div>
-            <div class="botao-padrao-cadastrar">
-                <a>
-                    <input name="btn_submit" type="submit" class="botao-cadastrar-submit" id="botao-cadastrar-submit" value="CADASTRAR">
-                </a>
-            </div>
-        </div>  
-    </section>
-
-
-</body>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-<link rel="stylesheet" href="../assets/css/validação_checklist.css">
+<script src="../assets/js/deletar_perfil.js"></script>
+
+<link rel="stylesheet" href="../assets/css/validar_checklist.css">
+
+
+
+<body class="tela_gerenciam_perfis">
+
+        <main class="pai-de-todos">
+            <form action="" method="GET">
+                <div class="container_gp">
+                    <h1 class="Perfis">Perfis</h1>
+                        <ul class="cardsgerenc">
+                            <li>
+                                <div class="div_card_perfil">
+                                    <div class="titulo_gp">
+                                        <div class="card_perfil">
+                                            <div class="card_nome">
+                                                <h2 class="tipo_perfil">AS COISAS NAO ESTAO CERTAS</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="botoes_direita">                        
+                                        <a class="bi bi-check-circle"> </a>
+                                         
+                                        <a><img src="../assets/imgs/icons/menu_icon_3.svg" alt="icone_editar" class="icone_menu"></a> 
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="titulo_gp">
+                                    <div class="card_perfil">
+                                        <div class="card_nome">
+                                            <h2 class="tipo_perfil">AS COISAS NAO ESTAO CERTAS</h2>
+                                        </div>
+                                            <a href="">
+                                                <img src="../assets/imgs/icons/icon_editar.png" alt="icone_editar" class="icone_editar"></a> 
+                                            <a class="bi bi-trash" ></a> 
+                                    </div>
+                                </div>
+                            </li> 
+
+                            <li>
+                                <div class="titulo_gp">
+                                    <div class="card_perfil">
+                                        <div class="card_nome">
+                                            <h2 class="tipo_perfil">AS COISAS NAO ESTAO CERTAS</h2>
+                                        </div>
+                                            <a href="">
+                                                <img src="../assets/imgs/icons/icon_editar.png" alt="icone_editar" class="icone_editar"></a> 
+                                            <a class="bi bi-trash"></a> 
+                                    </div>
+                                </div>
+                            </li> 
+
+                            <li>
+                                <div class="titulo_gp">
+                                    <div class="card_perfil">
+                                        <div class="card_nome">
+                                            <h2 class="tipo_perfil">AS COISAS NAO ESTAO CERTAS</h2>
+                                        </div>
+                                            <a href="">
+                                                <img src="../assets/imgs/icons/icon_editar.png" alt="icone_editar" class="icone_editar"></a> 
+                                            <a class="bi bi-trash" ></a> 
+                                    </div>
+                                </div>
+                            </li> 
+
+                            <li>
+                                <div class="titulo_gp">
+                                    <div class="card_perfil">
+                                        <div class="card_nome">
+                                            <h2 class="tipo_perfil">NOT DALE</h2>
+                                        </div>
+                                            <a href="">
+                                                <img src="../assets/imgs/icons/icon_editar.png" alt="icone_editar" class="icone_editar"></a> 
+                                            <a class="bi bi-trash" ></a> 
+                                    </div>
+                                </div>
+                            </li> 
+
+                        </ul>
+                
+                </div>
+                <div class="container_gp2">
+                        
+                    <div class="alinar-botoes">
+
+                        <div class="botao-padrao-voltar">
+                            <a href="listar_salas.php" class="botao-voltar-submit">VOLTAR</a>
+                        </div>
+
+                        <div class="botao-padrao-cadastrar">
+                            <a href="cadastrar_perfil.php" class="botao-cadastrar-link">CADASTRAR</a>
+                        </div>
+
+                    </div>  
+                        
+                </div> 
+            </form>
+        </main>
+    </body>
