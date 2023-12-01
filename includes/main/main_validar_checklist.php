ee838200b8f47aca9f724e246014c6adcd1cf79a
= <=>
2023-12-01 14:47:51 -0400

    asd
    sad

---
+++ includes/main/main_validar_checklist.php
@@ -0,0 +1,79 @@
+
+<body>
+    <section class="tudo">
+        
+        <div class="pagina">
+            <div class="titulo"> 
+                <h1>Validação do Checklist</h1>
+            </div>
+            
+            <div class="gear-second">
+                <div class="item">
+                    <span class="label">Professor:</span>
+                    <span class="value">Cristiane</span>
+                </div>
+                <div class="item">
+                    <span class="label">Ambiente:</span>
+                    <span class="value">Cozinha Didática I</span>
+                </div>
+            </div>
+
+            <div class="gear-third">
+                <div class="item">
+                    <span class="label">Data:</span>
+                    <span class="value">03/03/2023</span>
+                </div>
+
+                <div class="item">
+                    <span class="label">Aberto:</span>
+                    <span class="value">7:30 h</span>
+                </div>
+
+                <div class="item">
+                    <span class="label">Encerrado:</span>
+                    <span class="value">11:50 h</span>
+                </div>
+                </div>
+            
+            </div>
+
+            <div class="inputs">
+                <div class="area-check">
+                    <div class="input">                      
+                        <p class = "texto-checklist">
+                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
+                        </p>
+                    </div>
+                    <i class="bi bi-check-circle" id="check"></i>
+                </div>               
+                <div class="area-check">
+                    <div class="input">                      
+                        <p class = "texto-checklist">
+                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
+                        </p>
+                    </div>
+                    <i class="bi bi-x-circle" id="not-check"></i>
+                </div>
+                <div class="area-check">
+                    <div class="input">                      
+                        <p class = "texto-checklist">
+                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
+                        </p>
+                    </div>
+                    <i class="bi bi-check-circle" id="check"></i>
+                </div>
+                <div class="area-check">
+                    <div class="input">                      
+                        <p class = "texto-checklist">
+                            Os equipamentos e /ou utensílios estão limpos, organizados e em condições de uso?
+                        </p>
+                    </div>
+                    <i class="bi bi-check-circle" id="check"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+<link rel="stylesheet" href="../assets/css/validação_checklist.css">
