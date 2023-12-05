622c40879efb1793d5996961052242c9d36489fc
Joceyr Gomes Martins <joeymartins00@gmail.com>
2023-12-05 17:14:55 -0400

    A
    A

--- includes/main/main_visualizar_sala.php
+++ includes/main/main_visualizar_sala.php
@@ -1,46 +1,51 @@
 
+<!-- PARTE DA TELA QUE PEGA A COR DA BOX -->
 <style>
-        
         .card{
     border: solid  <?=$dados[0]['cor_itens']?>;
-}
-
+            }
 </style>
 
+<!-- LINK DO CSS "visualizar_sala.css" -->
 <link rel="stylesheet" href="../assets/css/visualizar_sala.css">
 
+
 <body class="visualizar_sala">
-            <div class="nome_da_sala" id="nome_da_sala">
-                <h1>Visualizar Sala</h1>
-            </div>
-        <section class="area_card">
+
+            <div class="nome_da_sala" id="nome_da_sala"> <h1>Visualizar Sala</h1> </div>
+        
+            <section class="area_card">
             
                 <div class="card">
+
+                    <!-- PARTE QUE PEGA A IMAGEM DA SALA -->
                     <div class="imagem_card">
-                            
                         <img src="../storage/salas/<?= $dados[0]['img_sala']?>" alt="" id="img_config">
                     </div>
+
+                    <!-- TUDO O QUE TA ESCRITO CARD-->
                     <div class="texto_card" >
 
-                        <!-- <div class="titulo_card"></div> -->
-                            <!-- <p class="label-sala"> <h1>Nome da Sala</h1></p> -->
-
+                            <!-- PARTE QUE PEGA O NOME DA SALA -->
                             <h1 class="nome-sala"><?=$dados[0]['nome']?></h1>
             
+                            <!-- DESCRIÇÃO -->
+                            <div class="paragrafo_card"> 
+                                
+                                <h2 id="paragrafo_descricao"> Descrição: </h2>
 
-                            <div class="paragrafo_card">
-                                <h2 id="paragrafo_descricao">
-                                    Descrição: 
-                                </h2>
-
+                                <!-- PARTE QUE PEGA A IMAGEM DA SALA -->
                                 <p class="texto-descricao"> <?=$dados[0]['descricao']?></p>
                                 
                                 
                             </div>
+
+                            <!-- DIAS DE FUNCIONAMENTO -->
                             <div class="dias-de-funcionamento">
-                                <h2 id="dias-funcionamento">
-                                    Dias de Funcionamento:
-                                </h2>
+                                
+                                <h2 id="dias-funcionamento"> Dias de Funcionamento: </h2>
+
+                                <!-- PARTE QUE PEGA OS DIAS DE FUNCIONAMENTO DO BANCO -->
 
                                 <p class="dias_funcionamento"> <?php
                                 
@@ -54,13 +59,14 @@
 
                                 ?></p>
                                 
-                                
                             </div>
-                            <div class="turnos-de-funcionamento">
-                                <h2 id="turnos-funcionamento">
-                                    Turnos de Funcionamento:
-                                </h2>
 
+                            <!-- TURNOS DE FUNCIONAMENTO -->
+                            <div class="turnos-de-funcionamento">
+
+                                <h2 id="turnos-funcionamento"> Turnos de Funcionamento: </h2>
+
+                                <!-- PARTE QUE PEGA OS TURNOS DE FUNCIONAMENTO DO BANCO -->
                                 <p class="turnos_funcionamento"> <?php
                                 
                                     echo $matutino.' ';
@@ -70,26 +76,21 @@
                                 
                                 ?></p>
                                 
-                                
-                            </div>
-                                                
+                            </div>                      
                     </div>
                 </div>
-        </section>
-        
-                <div class="alinhar_botoes">
+            </section>
+            
+            <!-- BOTÕES (voltar e fazer checklist)-->
+            <div class="alinhar_botoes">
 
-                    <!--Botão Voltar-->
-
-                        <a href="#"><input type="submit" class="botao-voltar-submit"  value="VOLTAR"></a>
-        
-                    <!--Botão Checklist -->
-
-                        <a href="../pages/cadastrar_checklist_preaula.php?id_sala=<?=$_GET['id_sala']?>"><input type="submit" class="botao-fazer-checklist-submit"  value="FAZER CHECKLIST"></a>
-
-                </div>
-        
+                <!--Botão Voltar-->
+                <a href="#"><input type="submit" class="botao-voltar-submit"  value="VOLTAR"></a>
     
+                <!--Botão Checklist -->
+                <a href="../pages/cadastrar_checklist_preaula.php?id_sala=<?=$_GET['id_sala']?>"><input type="submit" class="botao-fazer-checklist-submit"  value="FAZER CHECKLIST"></a>
+
+            </div>
 </body>
 
 
