08525fb358fc86f4bff576c620c1307b77eacffb
Arthur Simões <73851183+AsatoTexco@users.noreply.github.com>
2023-12-06 15:48:26 -0400

    0 bugs cad pergunta

--- pages/actions/cad_pergunta.php
+++ pages/actions/cad_pergunta.php
@@ -1,44 +1,7 @@
 <?php 
 
 require __DIR__."/../../vendor/autoload.php";
-use App\Entity\Pergunta;
-
-// if($_POST['antes_da_aula'] == 'on' && $_POST['depois_da_aula'] == 'on'){
-
-//     try{
-
-//         // cadastrar primeiro o pre 
-//         $obPergunta = new Pergunta($_POST['nova_pergunta']);
-//         $obPergunta->cadastrar('PRE');
-    
-//         // cadastrando o pos 
-//         $obPergunta->cadastrar('POS');
-
-        
-
-
-//     }catch(Exception $e){
-
-//         echo(json_encode($e->getMessage()));
-
-//     }
-// }elseif($_POST['antes_da_aula'] == 'on'){
-
-
-//     try{ 
-
-//         // cadastrar primeiro o pre 
-//         $obPergunta = new Pergunta($_POST['nova_pergunta']);
-//         $obPergunta->cadastrar('PRE'); 
-
-//     }catch(Exception $e){
-
-//         echo(json_encode($e->getMessage()));
-
-//     } 
-// }
-
-
+use App\Entity\Pergunta; 
 $obPergunta = new Pergunta($_POST['nova_pergunta']);
 
 if(isset($_POST['antes_da_aula'],$_POST['depois_da_aula'])){
@@ -57,6 +20,6 @@ if(isset($_POST['antes_da_aula'],$_POST['depois_da_aula'])){
 }
  
 
-echo(json_encode('cadastrou'));
+echo(json_encode($_POST));
 
 ?>
