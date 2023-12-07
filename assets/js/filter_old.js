4a6c782765125b0fb5e1725361729475e384f911
Arthur <arthur2006.teixeira@gmail.com>
2023-12-07 13:56:21 -0400

    cad pergunta 100%

--- assets/js/filter_old.js
+++ assets/js/filter_old.js
@@ -272,3 +272,12 @@ $("#botao_confirmar_editar").on('click',async function(e){
     }
 
 })
+
+
+$("#botao-nao-submit-excluir-pergunta").on('click',function(e){
+  e.preventDefault()
+  closePopup3()
+  closePopup1()
+
+
+})
