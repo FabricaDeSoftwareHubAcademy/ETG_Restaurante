bac6e4f36a93eea685cf4f4743fd23a8e223e9a2
FellypeAndre <84105223+LucasssBarbosa@users.noreply.github.com>
2024-01-15 17:31:18 -0400

    minha tela

---
+++ includes/main/main_listar_checklist_concluidas.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../assets/css/listar_checklist_concluidas.css">
+<body>
+    <main class="main_page">
+        <div class="container_title">
+            <h1>Cistar Checklist Concluidas</h1>
+        </div>
+
+    </main>
+</body>
---
+++ pages/listar_checklist_concluidas.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+$titulo_page = 'Mural Recados'; 
+include_once("../includes/menu.php");
+ 
+include_once("../includes/header/header.php");
+include_once("../includes/main/main_listar_checklist_concluidas.php");
