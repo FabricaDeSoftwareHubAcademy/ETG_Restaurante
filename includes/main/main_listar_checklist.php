897182792a01cd5ab747a908163e049150071417
= <=>
2024-01-17 15:09:08 -0400

    mds

--- includes/main/main_listar_checklist.php
+++ includes/main/main_listar_checklist.php
@@ -4,7 +4,10 @@
     
     <h1 class="titulo">Checklists fechados</h1>
         <form action="" class="cardsgerenc" method="GET">
+        <div class="container_gx p">
             <?=$imprimirChecklist?>
+        </div>
+
         </form>
         <div class="container_gp2">
                     
--- pages/listar_checklist.php
+++ pages/listar_checklist.php
@@ -20,24 +20,22 @@ foreach ($checklistData as $row) {
     
     $idChecklist = $row['id'];
     
-    $imprimirChecklist .= '<div class="container_gx p">
-                    <a href="validar_checklist.php?id_checklist='.$idChecklist.'" class="link_card">
-                            <div class="div_card_perfil">
-                                <div class="titulo_gp">
-                                    <div class="card_perfil">
-                                        <div class="imagem_card">
-                                            <img src="https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png" alt="">
-                                        </div>
-                                        <div class="card_nome">
-                                            <h2 class="tipo_perfil">Sala: '.$nomeSala.'</h2>
-                                            <h2 class="tipo_perfil">Abertura: '.$row['data_abertura'].'</h2>
-                                            <h2 class="tipo_perfil">Fechamento: '.$row['data_fechamento'].'</h2>
+    $imprimirChecklist .= '<a href="validar_checklist.php?id_checklist='.$idChecklist.'" class="link_card">
+                                <div class="div_card_perfil">
+                                    <div class="titulo_gp">
+                                        <div class="card_perfil">
+                                            <div class="imagem_card">
+                                                <img src="https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png" alt="">
+                                            </div>
+                                            <div class="card_nome">
+                                                <h2 class="tipo_perfil">Sala: '.$nomeSala.'</h2>
+                                                <h2 class="tipo_perfil">Abertura: '.$row['data_abertura'].'</h2>
+                                                <h2 class="tipo_perfil">Fechamento: '.$row['data_fechamento'].'</h2>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                    </a>
-                </div>';
+                            </a>';
 }
 // exit;
 
