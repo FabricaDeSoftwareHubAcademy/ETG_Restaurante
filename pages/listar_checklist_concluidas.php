f649d63a04b2dd3e834d4f5ee24997d17caf0afc
FellypeAndre <84105223+LucasssBarbosa@users.noreply.github.com>
2024-01-24 17:31:28 -0400

    banco

--- pages/listar_checklist_concluidas.php
+++ pages/listar_checklist_concluidas.php
@@ -3,31 +3,37 @@ session_start();
 include_once("../includes/menu.php");
 use App\Entity\Checklist;
 $checklist = Checklist::getDoneCheck();
-// var_dump($checklist[0]);
+var_dump($checklist);
+
+
 $list = '';
-foreach($checklist as $row){
-    $list .= '<a href="validar_checklist.php?id_realizacao='.$row['id_responder'].'" class="card">
-                <div class="card_img">
-                    <img src="../assets/img/sala.jpg" alt="foto da sala">
-                </div>
-                <div class="card_info">
-                    <div class="card_text">
-                        <div class="card_header_title">
-                            <h3>'.$row['nome_sala'].'</h3>
-                        </div>
-                        
-                        <div class="card_header_subtitle">
-                            <p>aberto às '.$row['data_abertura'].'</p>
-                            <p>fechado às '.$row['data_fechamento'].'</p>
-                        </div>
-
+foreach($checklist as $gp){
+    foreach($gp as $row){
+        $list .= '<a href="validar_checklist.php?id_realizacao='.$row['id_responder'].'" class="card">
+                    <div class="card_img">
+                        <img src="../assets/img/sala.jpg" alt="foto da sala">
                     </div>
-
-                </div>
-            </a>';
+                    <div class="card_info">
+                        <div class="card_text">
+                            <div class="card_header_title">
+                                <h3>'.$row['nome_sala'].'</h3>
+                            </div>
+                            
+                            <div class="card_header_subtitle">
+                                <p>aberto às '.$row['data_abertura'].'</p>
+                                <p>fechado às '.$row['data_fechamento'].'</p>
+                            </div>
+    
+                        </div>
+    
+                    </div>
+                </a>';
+    }
+    array_push($_GET,$list);
 }
+
+var_dump($_GET);
 
 
 include_once("../includes/header/header.php");
 include_once("../includes/main/main_listar_checklist_concluidas.php");
-?>
