be16d8e2b7e75789c5b649158a1f6f52bfe30dc9
FellypeAndre <84105223+LucasssBarbosa@users.noreply.github.com>
2024-01-30 17:20:47 -0400

    a

--- includes/main/main_listar_checklist_concluidas.php
+++ includes/main/main_listar_checklist_concluidas.php
@@ -5,36 +5,17 @@
         <div class="container_title">
             <h1>Listar Checklist Encerrada</h1>
         </div>
-        <!-- <section class='search'>
-            <div class="search_input">
-                <input type="text" placeholder="Pesquisar">
-            </div>
-            <div class="search_btn">
-                <button>Pesquisar</button>
-            </div>
-        </section> -->
         <section class="container">
-            <section class="lista">
-                <!-- <a href="#" class="card">
-                    <div class="card_img">
-                        <img src="" alt="foto da sala">
+                <section class='search'>
+                    <div class="search_input">
+                        <input type="text" placeholder="Pesquisar">
                     </div>
-                    <div class="card_info">
-                        <div class="card_text">
-                            <div class="card_header_title">
-                                <h3>sala 1</h3>
-                            </div>
-                            
-                            <div class="card_header_subtitle">
-                                <p>aberto às 10:10:10</p>
-                                <p>fechado às 10:10:10</p>
-                            </div>
-
-                        </div>
-
+                    <div class="search_btn">
+                        <button>Pesquisar</button>
                     </div>
-                </a> -->
-                <?php 
+                </section>
+                <section class="lista">
+                    <?php 
                 if(!isset($_GET['pagina'])){
                     $_GET['pagina'] = 0;
                 }
--- pages/listar_checklist_concluidas.php
+++ pages/listar_checklist_concluidas.php
@@ -14,9 +14,11 @@ foreach($checklist as $gp){
     foreach($gp as $row){
         // var_dump($row);
         // echo "<br><br>";
+        $row['data_abertura'] = date('d/m/Y H:i', strtotime($row['data_abertura']));
+        $row['data_fechamento'] = date('d/m/Y H:i', strtotime($row['data_fechamento']));    
         $list .= '<a href="validar_checklist.php?id_realizacao='.$row['id_responder'].'" class="card">
                     <div class="card_img">
-                        <img src="../assets/img/sala.jpg" alt="foto da sala">
+                        <img src="../assets/img/'.$row['img_sala'].'" alt="foto da sala">
                     </div>
                     <div class="card_info">
                         <div class="card_text">
