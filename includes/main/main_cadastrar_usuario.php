7439ae0bf27d18685df9f2d58cc0e8c52bb4a085
Igor <igoralbieri04@gmail.com>
2024-02-02 15:16:12 -0400

    egfdgfdghbfs

--- includes/main/main_cadastrar_usuario.php
+++ includes/main/main_cadastrar_usuario.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="../assets/css/cadastrar_usuario.css">
-<link rel="stylesheet" href="../assets/css/estilo_botoes_padronizados.css">
+<!-- <link rel="stylesheet" href="../assets/css/estilo_botoes_padronizados.css"> -->
 <!-- <link rel="stylesheet" href="../modais/usuario_cadastrado.php"> -->
 
 <body class="Pai-de-todos">
     
     <main class="tudo_esqueceu_senha1">
-        <form method="POST">
+        <form method="POST" id="form_cad">
             <section class='titulo_cadastro_usuario'>
                 <h1>Cadastrar Usuário</h1>
             </section>
@@ -56,7 +56,7 @@
             
             <!--Botão Confirmar-->
             <div class="botao-padrao-confirmar">
-                    <a><input type="submit" class="botao-confirmar-submit" name="btn_submit" value="CONFIRMAR"></a>
+                    <a><input type="submit" class="botao-confirmar-submit" id="botao-confirmar-submit" name="btn_submit" value="CONFIRMAR"></a>
                 </div>
             </section>
             
@@ -65,32 +65,60 @@
     
 
 
-    <script>
+<script>
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const nomes = document.getElementById('nomes').value;
-        const emails = document.getElementById('emails').value;
-        const options = document.getElementById('options').value;
-        const matricula = document.getElementById('matricula').value;
-        const senhas = document.getElementById('senhas').value;
-    
-        if (nomes.length == 0) {
-            console.log("Campo de nome não preenchido");
-        } 
-        // else (nomes.length > 0) {
-        //     console.log("Campo de nome preenchido");
-        // } 
+let button =document.getElementById('botao-confirmar-submit')
         
+button.addEventListener('click', async (e) =>{
+        
+    e.preventDefault()
+    
+    const nomes = document.getElementById('nomes').value;
+    const emails = document.getElementById('emails').value;
+    const options = document.getElementById('options').value;
+    const matricula = document.getElementById('matricula').value;
+    const senhas = document.getElementById('senhas').value;
+    // console.log(nomes);
+    
+    // if (nomes.length > 0) {
+    //     console.log("Campo de nome preenchido");
+    // } 
+    // else{
+    //     console.log("Campo de nome não preenchido");
+    // } 
+    
+    if (nomes.length > 0 && emails.length > 0 && options.length > 0 && matricula.length > 0 && senhas.length > 0) {
+        console.log("Todos os campos estão preenchidos.");
 
-        // if (nomes.length == 0 || emails.length == 0 || options.length == 0 || matricula.length == 0 || senhas.length == 0) {
-        //     console.log("Todos os campos estão preenchidos.");
-        // } 
-        // else {
-        //     console.log("Preencha todos os campos.");
-        // }
-    });
+        let form = document.getElementById('form_cad')
+        console.log(form)
 
-    </script>
+        let formData = new FormData(form)
+        let dados_php = await fetch("../pages/actions/cad_usuario.php",{method:"POST",
+            body: formData
+        })
+
+        let response = await dados_php.json()
+
+        console.log(response)
+
+        if(response){
+
+            console.log("funfou")
+
+        }
+        else{
+            console.log("nao funfou")
+        }
+
+
+    } else {
+        console.log("Preencha todos os campos.");
+    }
+
+});
+
+</script>
     
 
     
