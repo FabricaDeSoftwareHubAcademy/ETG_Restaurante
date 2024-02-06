ddb3239ae7181504ccdaf451e7cd4488c5061e5d
FellypeAndre <84105223+LucasssBarbosa@users.noreply.github.com>
2024-02-06 17:32:02 -0400

    a

--- pages/visualizar_sala.php
+++ pages/visualizar_sala.php
@@ -18,31 +18,31 @@ if(isset($_GET['id_sala'])){
 } 
 
 $btn_checklist = ''; 
-if(Checklist::getLastCheck($id_sala)["data_fechamento"] == null){
-    $btn_checklist = '
-    <div class="botao-padrao-fazer-checklist">
-        <a href="../pages/cadastrar_checklist_preaula.php?id_sala='.$_GET['id_sala'].'"><input type="submit" class="botao-fazer-checklist-submit"  value="FAZER CHECKLIST"></a>
-    </div>';
-}else{
-    
-    if(Checklist::getLastCheck($id_sala)["data_fechamento"] != null){
-    
+if($ifreacheck){
+    if(Checklist::getLastCheck($id_sala)["data_fechamento"] == null){
         $btn_checklist = '
         <div class="botao-padrao-fazer-checklist">
             <a href="../pages/cadastrar_checklist_preaula.php?id_sala='.$_GET['id_sala'].'"><input type="submit" class="botao-fazer-checklist-submit"  value="FAZER CHECKLIST"></a>
         </div>';
-    
-    }else{ 
-    
-        $btn_checklist = '
-        <div class="botao-padrao-fazer-checklist">
-            <a href="../pages/cadastrar_checklist_posaula.php?id_sala='.$_GET['id_sala'].'"><input type="submit" class="botao-fazer-checklist-submit"  value="PÓS AULA"></a>
-        </div>';
-    
+    }else{
+        
+        if(Checklist::getLastCheck($id_sala)["data_fechamento"] != null){
+        
+            $btn_checklist = '
+            <div class="botao-padrao-fazer-checklist">
+                <a href="../pages/cadastrar_checklist_preaula.php?id_sala='.$_GET['id_sala'].'"><input type="submit" class="botao-fazer-checklist-submit"  value="FAZER CHECKLIST"></a>
+            </div>';
+        
+        }else{ 
+        
+            $btn_checklist = '
+            <div class="botao-padrao-fazer-checklist">
+                <a href="../pages/cadastrar_checklist_posaula.php?id_sala='.$_GET['id_sala'].'"><input type="submit" class="botao-fazer-checklist-submit"  value="PÓS AULA"></a>
+            </div>';
+        
+        }
     }
 }
-
-
 
 // print_r($dados);
 // echo($dados['funcionamento']);
