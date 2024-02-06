f1546b8e53db9e15d77c8c9b1db83ee69361af49
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2024-02-06 17:03:45 -0400

    yi

--- pages/actions/cat_data_pergunta.php
+++ pages/actions/cat_data_pergunta.php
@@ -7,7 +7,7 @@ use App\Entity\Sala;
 $id_sala = $_GET['id_sala'];
 
 $dados = json_decode(file_get_contents('php://input'), true);
-// echo(json_encode($dados));exit;
+
 
 $obj_sala = new Sala(); 
 $dados_sala = $obj_sala::getDadosById($id_sala);
@@ -15,4 +15,6 @@ $id_check = $dados_sala[0]['id_check'];
  
 
 $response=ResponderChecklist::cadastrar($dados, $id_sala, $id_check);
+
+echo(json_encode(true));
  
