2a9bf6ffc6abebc452e36e9d8dc03e8d639a351a
Igor <igoralbieri04@gmail.com>
2024-02-16 17:24:18 -0400

    aaa

--- includes/main/main_cadastrar_usuario.php
+++ includes/main/main_cadastrar_usuario.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" href="../assets/css/cadastrar_usuario.css">
+<script src="../assets/js/modais.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <!-- <link rel="stylesheet" href="../assets/css/estilo_botoes_padronizados.css"> -->
 <!-- <link rel="stylesheet" href="../modais/usuario_cadastrado.php"> -->
 
@@ -72,73 +74,72 @@
         
 
 
-<script>
+    <script>
 
-let button =document.getElementById('botao-confirmar-submit')
+    let button =document.getElementById('botao-confirmar-submit')
+            
+    button.addEventListener('click', async (e) =>{
+            
+        e.preventDefault()
         
-button.addEventListener('click', async (e) =>{
+        const nomes = document.getElementById('nomes').value;
+        const emails = document.getElementById('emails').value;
+        const options = document.getElementById('options').value;
+        const matricula = document.getElementById('matricula').value;
+        const senhas = document.getElementById('senhas').value;
+        // console.log(nomes);
         
-    e.preventDefault()
-    
-    const nomes = document.getElementById('nomes').value;
-    const emails = document.getElementById('emails').value;
-    const options = document.getElementById('options').value;
-    const matricula = document.getElementById('matricula').value;
-    const senhas = document.getElementById('senhas').value;
-    // console.log(nomes);
-    
-    // if (nomes.length > 0) {
-    //     console.log("Campo de nome preenchido");
-    // } 
-    // else{
-    //     console.log("Campo de nome não preenchido");
-    // } 
-    
-    if (nomes.length > 0 && emails.length > 0 && options.length > 0 && matricula.length > 0 && senhas.length > 0) {
+        // if (nomes.length > 0) {
+        //     console.log("Campo de nome preenchido");
+        // } 
+        // else{
+        //     console.log("Campo de nome não preenchido");
+        // } 
+        
+        if (nomes.length > 0 && emails.length > 0 && options.length > 0 && matricula.length > 0 && senhas.length > 0) {
 
-        if(emails.includes('@')){ 
+            if(emails.includes('@')){ 
 
-            let form = document.getElementById('form_cad')
-              
-            let formData = new FormData(form)
-            let dados_php = await fetch("../pages/actions/action_cadastrar_user.php",{
-                method:"POST",
-                body: formData
-            }) 
-            let response = await dados_php.json() 
-    
-            if(response.status){ 
+                let form = document.getElementById('form_cad')
+                
+                let formData = new FormData(form)
+                let dados_php = await fetch("../pages/actions/action_cadastrar_user.php",{
+                    method:"POST",
+                    body: formData
+                }) 
+                let response = await dados_php.json() 
+        
+                if(response.status){ 
 
-                $('#nomes').val('')
-                $('#emails').val('')
-                $('#matricula').val('')
-                $('#senhas').val('') 
+                    $('#nomes').val('')
+                    $('#emails').val('')
+                    $('#matricula').val('')
+                    $('#senhas').val('') 
 
 
 
-                modalStatus('Usuário cadastrado com Sucesso!','success')
-    
+                    modalStatus('Usuário cadastrado com Sucesso!','success')
+        
+                }
+                else{
+                    modalStatus('Ocorreu um erro, tente novamente mais tarde!','error')
+                }
+
+            }else{
+
+                modalStatus('Insira um E-mail Válido!','error')
+
+
             }
-            else{
-                modalStatus('Ocorreu um erro, tente novamente mais tarde!','error')
-            }
-
-        }else{
-
-            modalStatus('Insira um E-mail Válido!','error')
+        
 
 
+        } else {
+            modalStatus('Preencha todos os campos!','error')
         }
-     
 
+    });
 
-    } else {
-        modalStatus('Preencha todos os campos!','error')
-    }
-
-});
-
-</script>
- 
-
-</body>
+    </script>
+    
+ </body>
