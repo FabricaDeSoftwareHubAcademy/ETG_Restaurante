866e884ed526c23d9b055d9d81572ee535267fc3
vINI <124309138+FellypeAndre@users.noreply.github.com>
2024-02-16 15:20:52 -0400

    Update cadastrar_checklist.php

--- pages/cadastrar_checklist.php
+++ pages/cadastrar_checklist.php
@@ -7,18 +7,18 @@ if(!$ifgencheck){
     header('Location: ./listar_recados.php');
 }
 
-// use App\Entity\Pergunta;
-// use App\Entity\CadastroChecklist;
-// use App\Entity\Funcoes;
+use App\Entity\Pergunta;
+use App\Entity\CadastroChecklist;
+use App\Entity\Funcoes;
 
 
  
 
-// if (isset($_POST['btn_cadastrar']) && isset($_POST['nome_checklist'])) {
-//     $check = new CadastroChecklist();
-//     $check->nome = $_POST['nome_checklist'];
-//     $id = $check->cadastrar($_POST['perguntas']);
-// }
+
+$check = new CadastroChecklist();
+$check->nome = $_POST['nome_checklist'];
+$id = $check->cadastrar($_POST['perguntas']);
+
  
 
 
