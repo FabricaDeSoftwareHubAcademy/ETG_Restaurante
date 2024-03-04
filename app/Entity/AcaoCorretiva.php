4e336719483484029781bec9e52bf3ab0aa4fe52
= <=>
2024-03-04 17:39:37 -0400

    a
    a

--- app/Entity/AcaoCorretiva.php
+++ app/Entity/AcaoCorretiva.php
@@ -9,28 +9,16 @@ use Exception;
 class AcaoCorretiva 
 {
     
-    private string $id;
-    private string $id_nao_conformidade;
-    private string $descricao;
-    private string $img_1;
-    private string $img_2;
-    private string $img_3;
+    private $id;
+    private $id_nao_conformidade;
+    private $descricao;
+    private $img_1;
+    private $img_2;
+    private $img_3;
     private Banco $conn;
 
 
-    public function __construct(
-        string $id_nao_conformidade,
-        string $descricao,
-        string $img_1,
-        string $img_2 = "",
-        string $img_3 = ""
-    ) {
-        $this->$id_nao_conformidade = $id_nao_conformidade;
-        $this->$descricao = $descricao;
-        $this->$img_1 = $img_1;
-        $this->$img_2 = $img_2;
-        $this->$img_3 = $img_3;
-
+    public function __construct() {
         $this->conn = new Banco("reg_correcao");
     }
     
@@ -39,9 +27,9 @@ class AcaoCorretiva
             $dados = [
                 "reg_NC_id" => $this->id_nao_conformidade,
                 "descricao" => $this->descricao,
-                "img_1" => $this->img_1,
-                "img_2" => $this->img_2,
-                "img_3" => $this->img_3
+                "img1" => $this->img_1,
+                "img2" => $this->img_2,
+                "img3" => $this->img_3
             ];
             $this->conn->insert($dados);
             return true;
@@ -51,6 +39,10 @@ class AcaoCorretiva
         catch (Exception $e) {
             throw $e->getMessage();
         }
+    }
+
+    public function setIdNaoConformidade(String $id_nao_conformidade) {
+        $this->id_nao_conformidade = $id_nao_conformidade;
     }
 
     public function setDescricao($descricao) {
--- pages/actions/action_cadastrar_acao_corretiva.php
+++ pages/actions/action_cadastrar_acao_corretiva.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function convert_base64_png(String $img) {
+function convert_base64_png($img) {
     //LOGICA DA IMAGEM
     list($type, $data) = explode(';', $img);
     list(, $data) = explode(',', $data); 
@@ -17,37 +17,42 @@ function convert_base64_png(String $img) {
 }
 
 $data = json_decode(file_get_contents("php://input"), true);
-// $response = ["status" => true, "data" => $data];
+$response = ["status" => true];
+
+$objeto_acao_corretiva = new AcaoCorretiva();
 
 foreach ($data[0] as $AcaoCorretiva) {
     $id_nao_conformidade = $AcaoCorretiva["id_nao_conformidade"];
     $descricao = $AcaoCorretiva["textAreaContent"];
-    $img_1 = $AcaoCorretiva["imagesToPHP"][0];
-    // $img_2 = $AcaoCorretiva["imagesToPHP"][1];
-    // $img_3 = $AcaoCorretiva["imagesToPHP"][2];
+    $img_1 = ($AcaoCorretiva["imagesToPHP"][0] != "") ? $AcaoCorretiva["imagesToPHP"][0] : "";
+    $img_2 = ($AcaoCorretiva["imagesToPHP"][1] != "") ? $AcaoCorretiva["imagesToPHP"][1] : "";
+    $img_3 = ($AcaoCorretiva["imagesToPHP"][2] != "") ? $AcaoCorretiva["imagesToPHP"][2] : "";
 
     $img_1 = ($img_1 != "") ? convert_base64_png($img_1) : "";
-    // $img_2 = ($img_2 != "") ? convert_base64_png($img_2) : "";
-    // $img_3 = ($img_3 != "") ? convert_base64_png($img_3) : "";
-    
-    
-    $objeto_acao_corretiva = new AcaoCorretiva($id_nao_conformidade, $descricao, $img_1);
-    echo json_encode($img_1);
-    // $id_nao_conformidade,
-    // $descricao,
-    // $img_1,
-    // $img_2,
-    // $img_3?
+    $img_2 = ($img_2 != "") ? convert_base64_png($img_2) : "";
+    $img_3 = ($img_3 != "") ? convert_base64_png($img_3) : "";
 
-    // $responseDB = $objeto_acao_corretiva->save();
-
-    // if(!$responseDB) {
-    //     //ARMAZENAR NO LOG
-    //     $response = ["status" => false];
-    //     break;
-    // }
+    // $teste = [];
+    // $teste["img1"] = $img_1;
+    // $teste["img2"] = $img_2;
+    // $teste["img3"] = $img_3;
+    
+    $objeto_acao_corretiva->setIdNaoConformidade($id_nao_conformidade);
+    $objeto_acao_corretiva->setDescricao($descricao);
+    $objeto_acao_corretiva->setImg_1($img_1);
+    $objeto_acao_corretiva->setImg_2($img_2);
+    $objeto_acao_corretiva->setImg_3($img_3);
+    
+    $responseDB = $objeto_acao_corretiva->save();
+    
+    if(!$responseDB) {
+                //ARMAZENAR NO LOG
+                $response = ["status" => false];
+                break;
+            }
 }
-
+echo json_encode($response["status"]);
+    
 
 
 
