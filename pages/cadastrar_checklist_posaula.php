c1da76590e284687c87fdac90134a1c111596bef
Arthur Simões <73851183+AsatoTexco@users.noreply.github.com>
2024-03-07 15:06:14 -0400

    arrumando listar_salas

--- pages/cadastrar_checklist_posaula.php
+++ pages/cadastrar_checklist_posaula.php
@@ -10,10 +10,10 @@ if(!$ifreacheck){
 use App\Entity\Checklist;
 use App\Entity\Pergunta;
 use App\Entity\Sala;
-$cond = Sala::getDados('responsavel, status','id = '.$_GET['id_sala']);
-if(($_SESSION['id_user'] != $cond[0]['responsavel'] && !$ifreaac) || $cond[0]['status'] != 'A'){
-    header("Location: ./listar_recados.php");
-}
+// $cond = Sala::getDados('responsavel, status','id = '.$_GET['id_sala']);
+// if(($_SESSION['id_user'] != $cond[0]['responsavel'] && !$ifreaac) || $cond[0]['status'] != 'A'){
+//     header("Location: ./listar_recados.php");
+// }
 $obj_checklist = new Checklist();
 $obj_pergunta = new Pergunta();
 $dados_imprimir = "";
