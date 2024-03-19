6f275e6e883c63474cccdf4513dbf42b12398125
vINI <124309138+FellypeAndre@users.noreply.github.com>
2024-03-19 17:41:14 -0400

    att

--- includes/main/teste.php
+++ includes/main/teste.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="../assets/js/modais"></script>
-</head>
-<body>
-    
-    Ola Mundo!
+<?php
 
 
-</body>
-</html>
+if(isset($_GET['parametro'])){
+
+    $x = $_GET['parametro'];
+
+    echo $x;
+
+    if($x == 'true'){
+        echo "es verdaded cabron";
+    }
+
+
+}else{
+    echo "No paramet";
+}
+
+
