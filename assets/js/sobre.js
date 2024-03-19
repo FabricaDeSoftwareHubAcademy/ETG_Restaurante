08b3a1971ecc2e406e71199229c88a258620f6f8
Igor <igoralbieri04@gmail.com>
2024-03-19 15:36:38 -0400

    HAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -7,17 +7,17 @@ let timeDom = document.querySelector('.carousel .time');
 
 
 let photos = [
-    { src: "../assets/imgs/devs/andre.jpg", author: "Fabrica de Software 276", title: "André Fellype", topic: "FullStack",   description: "Um programador full-stack entusiasta por códigos de programação e pela metodologia de desenvolvimento Scrum. No primeiro semestre de 2024, estarei finalizando meu curso técnico em Desenvolvimento de Sistemas ministrado pelo SENAC/MS e em 2025 finalizarei a graduação em Sistemas para Internet no IFMS - Instituto Federal de Mato Grosso do Sul", link: "https://www.linkedin.com/in/andr%C3%A9-fellype-634594207/"},
-    { src: "../assets/imgs/devs/frank_corr.jpg", author: "Fabrica de Software 276", title: "Arthur Frantz", topic: "FullStack",  description: "Aficionado por criar experiências incríveis, do front ao back. Sempre buscando novas soluções e melhorias.", link:"https://www.linkedin.com/in/arthurfrantz/"  },
-    { src: "../assets/imgs/devs/simoes.jpeg", author: "Fabrica de Software 276", title: "Arthur Simões", topic: "FullSatack",   description: "Olá, meu nome é Arthur Simões, tenho 18 anos e sou um entusiasta da Tecnologia. Desde sempre, tenho sido fascinado por essa área. Atuo no desenvolvimento de sites e projetos tanto front-end quanto back-end, além de contribuir ativamente para a comunidade Open Source no GitHub e em projetos livres, como Bots e APIs.", link: "https://www.linkedin.com/in/arthur-sim%C3%B5es/" },
-    { src: "../assets/imgs/devs/Guilherme.jpeg", author: "Fabrica de Software 276", title: "Guilherme Schuindt", topic: "ANIMAL",   description: "#",link: "#" },
-    { src: "../assets/imgs/devs/Igor.jpeg", author: "Fabrica de Software 276", title: "Igor Savioli", topic: "FullStack",    description: "Apaixonado por desafios que agregam conhecimento. Com uma sólida experiência tanto no desenvolvimento front-end quanto no back-end, estou constantemente em busca de oportunidades que me permitam expandir meu conjunto de habilidades e me manter atualizado com as últimas tecnologias e tendências do setor.", link: "https://www.linkedin.com/in/igor-savioli-albieri-766525265/"},
-    { src: "../assets/imgs/devs/joceyr.jpg", author: "Fabrica de Software 276", title: "Joceyr Martins", topic: "ANIMAL",    description: "#", link: "https://www.linkedin.com/in/joceyr-gomes-martins/"},
-    { src: "../assets/imgs/devs/leandro.jpeg", author: "Fabrica de Software 276", title: "Leandro Cavalcante", topic: "Front-end",   description: "#", link: "#" },
-    { src: "../assets/imgs/devs/lluiz.jpg", author: "Fabrica de Software 276", title: "Luiz Chimenes", topic: "Dev'Ops",   description: "Aficionado por tecnologia e colaboração.Foco em automação, entrega continua e integração entre dev e ops. Sempre aprendendo e experimentando.Café é vida", link: "https://www.linkedin.com/in/luiz-guilherme-chimenes-56927b253/" },
-    { src: "../assets/imgs/devs/thays.jpeg", author: "Fabrica de Software 276", title: "Thays Martines", topic: "Ui Ux Designer/Front-end",   description: "Tenho um forte domínio das áreas de front-end, especialmente na criação da parte visual de produtos. Além disso, tenho uma paixão pela área de Product Owner, onde posso moldar a visão do produto e trabalhar diretamente com as necessidades dos clientes. Nas horas vagas, também me aventuro no design e na criação de protótipos", link: "https://www.linkedin.com/in/thays-martines-622161214/"},
-    { src: "../assets/imgs/devs/Vinicius.jpeg", author: "Fabrica de Software 276", title: "Vinicius Martins", topic: "ANIMAL",  description: "#", link: "#"},
-    { src: "#", author: "Fabrica de Software 276", title: "Wanderley Terra", topic: "Back-end",  description: "#", link: "#"}
+    { src: "../assets/imgs/devs/andre.jpg", author: "Fabrica de Software 276", title: "André Fellype", topic: "FullStack",   description: "Um programador full-stack entusiasta por códigos de programação e pela metodologia de desenvolvimento Scrum. No primeiro semestre de 2024, estarei finalizando meu curso técnico em Desenvolvimento de Sistemas ministrado pelo SENAC/MS e em 2025 finalizarei a graduação em Sistemas para Internet no IFMS - Instituto Federal de Mato Grosso do Sul", link: "https://www.linkedin.com/in/andr%C3%A9-fellype-634594207/", link2: "https://github.com/FellypeAndre"},
+    { src: "../assets/imgs/devs/frank_corr.jpg", author: "Fabrica de Software 276", title: "Arthur Frantz", topic: "FullStack",  description: "Aficionado por criar experiências incríveis, do front ao back. Sempre buscando novas soluções e melhorias.", link:"https://www.linkedin.com/in/arthurfrantz/", link2: "https://github.com/ArthurSkl"},
+    { src: "../assets/imgs/devs/simoes.jpeg", author: "Fabrica de Software 276", title: "Arthur Simões", topic: "FullSatack",   description: "Olá, meu nome é Arthur Simões, tenho 18 anos e sou um entusiasta da Tecnologia. Desde sempre, tenho sido fascinado por essa área. Atuo no desenvolvimento de sites e projetos tanto front-end quanto back-end, além de contribuir ativamente para a comunidade Open Source no GitHub e em projetos livres, como Bots e APIs.", link: "https://www.linkedin.com/in/arthur-sim%C3%B5es/", link2: "https://github.com/AsatoTexco"},
+    { src: "../assets/imgs/devs/Guilherme.jpeg", author: "Fabrica de Software 276", title: "Guilherme Schuindt", topic: "ANIMAL",   description: "#",link: "#", link2:"https://github.com/xuintz" },
+    { src: "../assets/imgs/devs/Igor.jpeg", author: "Fabrica de Software 276", title: "Igor Savioli", topic: "Front-end",    description: "Apaixonado por desafios que agregam conhecimento. Com uma sólida experiência tanto no desenvolvimento front-end quanto no back-end, estou constantemente em busca de oportunidades que me permitam expandir meu conjunto de habilidades e me manter atualizado com as últimas tecnologias e tendências do setor.", link: "https://www.linkedin.com/in/igor-savioli-albieri-766525265/", link2: "https://github.com/Igoncio"},
+    { src: "../assets/imgs/devs/joceyr.jpg", author: "Fabrica de Software 276", title: "Joceyr Martins", topic: "Front-ent",    description: "sou graduado em Tecnologia em Segurança Pública, atualmente sou Coordenador de Programação de TV na RMC - Rede Mato Grossense de Televisão - TV Morena afiliada da Rede Globo. Sou apaixonado por tecnologia, e estou cada dia mais aprendendo e me aperfeiçoando no Desenvolvimento Front-End. Pretendo transitar de carreira, e trabalhar nessa área como DevWeb. ", link: "https://www.linkedin.com/in/joceyr-gomes-martins/", link2: "https://github.com/joeymartins00"},
+    { src: "../assets/imgs/devs/leandro.jpeg", author: "Fabrica de Software 276", title: "Leandro Cavalcante", topic: "Front-end",   description: "#", link: "#", link2: "https://github.com/leandro2089" },
+    { src: "../assets/imgs/devs/lluiz.jpg", author: "Fabrica de Software 276", title: "Luiz Chimenes", topic: "Dev'Ops",   description: "Aficionado por tecnologia e colaboração.Foco em automação, entrega continua e integração entre dev e ops. Sempre aprendendo e experimentando.Café é vida", link: "https://www.linkedin.com/in/luiz-guilherme-chimenes-56927b253/", link2: "https://github.com/ChimenesLuiz"},
+    { src: "../assets/imgs/devs/thays.jpeg", author: "Fabrica de Software 276", title: "Thays Martines", topic: "Ui Ux Designer/Front-end",   description: "Tenho um forte domínio das áreas de front-end, especialmente na criação da parte visual de produtos. Além disso, tenho uma paixão pela área de Product Owner, onde posso moldar a visão do produto e trabalhar diretamente com as necessidades dos clientes. Nas horas vagas, também me aventuro no design e na criação de protótipos", link: "https://www.linkedin.com/in/thays-martines-622161214/", link2: "https://github.com/thaysmartines"},
+    { src: "../assets/imgs/devs/Vinicius.jpeg", author: "Fabrica de Software 276", title: "Vinicius Martins", topic: "ANIMAL",  description: "#", link: "#", link2:"https://github.com/vINIMRZZ5"},
+    { src: "#", author: "Fabrica de Software 276", title: "Wanderley Terra", topic: "Back-end",  description: "#", link: "#", link2: "https://github.com/WanderTerra"}
 ];
 
 
@@ -35,7 +35,7 @@ function addPhotoToSlider(photo) {
             <div class="des">${photo.description}</div> <!-- Aqui exibimos a descrição correspondente -->
             <div class="buttons">
                 <a class="link-contato" href="${photo.link}"><i id="icon" class="bi bi-linkedin"></i></a>
-                <a class="link-contato" href="#"><i id="icon" class="bi bi-github"></i></a>
+                <a class="link-contato" href="${photo.link2}"><i id="icon" class="bi bi-github"></i></a>
                 <a class="link-contato" href="#"><i id="icon" class="bi bi-envelope-at-fill"></i></a>
             </div>
   
@@ -47,7 +47,7 @@ function addPhotoToSlider(photo) {
 function updateThumbnailGallery() {
     thumbnailBorderDom.innerHTML = ""; 
 
-    for (let i = currentIndex; i < currentIndex + 4; i++) {
+    for (let i = currentIndex; i < currentIndex + 5; i++) {
         let index = i % photos.length; 
         let photo = photos[index]; 
 
