a81eb4f92fc77c2fac0ef1ec9163038fff354b77
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2024-03-20 16:16:45 -0400

    Revert commit andre

--- assets/js/modal_pergunta_checklist.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById('form_cad_pergunta')
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    const formData = new FormData(form)
-    console.log(formData)
-})
