7b8f38b6a28bc23434358a02c97f0bcd9d860c4f
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2024-03-28 15:16:20 -0400

    sadsad
    sadsadsa

--- pages/visualizar_sala.php
+++ pages/visualizar_sala.php
@@ -4,21 +4,12 @@ require __DIR__."/../vendor/autoload.php";
 include_once("../includes/menu.php"); 
 $titulo_page = 'Visualizar Sala';
 require("../includes/header/header.php");
-
-
- 
 use App\Entity\Sala;
 use App\Entity\Checklist;
-
- 
 if(isset($_GET['id_sala'])){
-    
     $id_sala = $_GET['id_sala'];
-
     $dados = Sala::getDadosById($id_sala);
- 
 }
-
 $btn_checklist = '';
 
 $objCheck = new Checklist();
@@ -26,18 +17,12 @@ $teste = $objCheck->getLastCheck($id_sala);
 $status = Sala::getDados(col:'status,responsavel',where:'id = '.$id_sala);
 $responsavel=$status[0]["responsavel"];
 
-
 $data_hora = $objCheck->validar_40_minutos($id_sala,$responsavel);
 var_dump($data_hora);
 $data_abertura=$data_hora[0]["data_abertura"];
 // var_dump($data_hora[0]["NOW()"]);
 $hora_atual=$data_hora[0]["NOW()"];
-var_dump($hora_atual);
-
-
-
-
-
+// var_dump($hora_atual);
 
 
 // Duas timestamps de exemplo
@@ -51,13 +36,6 @@ $diferenca_segundos = abs($timestamp2 - $timestamp1);
 $diferenca_minutos = $diferenca_segundos / 60;
 
 // Verifica se a diferença é maior ou igual a 40 minutos
-// if ($diferenca_minutos >= 40) {
-//     echo "A diferença entre as timestamps é igual ou maior que 40 minutos.";
-// } 
-
-
-
-
 
 //A partir da qui é antes do copiar e colar. 
 
@@ -70,15 +48,12 @@ if($status[0]['status']=='L'){
     </div>';
 }
 elseif($status[0]['status']=='A'){
-    
-
-
     // Aqui está uma gambiarra para resolver diferença de hora kkkkkkk
     // print($diferenca_minutos);
     $diferenca_minutos=$diferenca_minutos-60;
     print($diferenca_minutos);
     if(($status[0]['responsavel'] == $_SESSION['id_user'] || $ifreaac) && $diferenca_minutos >= 40 ){
-        print("entrou no if ");
+        // print("entrou no if ");
         $btn_checklist = '
                 <div class="botao-padrao-fazer-checklist">
                     <a href="../pages/cadastrar_checklist_posaula.php?id_sala='.$_GET['id_sala'].'"><input type="submit" class="botao-fazer-checklist-submit"  value="PÓS AULA"></a>
@@ -90,10 +65,7 @@ elseif($status[0]['status']=='A'){
     };
     
 }
-
 $funcionamento = json_decode($dados[0]['horarios'], true);
-// print_r($funcionamento);
-
 $segunda = $funcionamento['segunda'] == 'sim' ? 'Segunda' : '';
 $terca = $funcionamento['terca'] == 'sim' ? 'Terça' : '';
 $quarta = $funcionamento['quarta'] == 'sim' ? 'Quarta' : '';
@@ -103,7 +75,6 @@ $sabado = $funcionamento['sabado'] == 'sim' ? 'Sábado' : '';
 $matutino = $funcionamento['turnos']['matutino'] == 'sim' ? 'Matutino' : ' ';
 $vespertino = $funcionamento['turnos']['vespertino'] == 'sim' ? 'Vespertino' : ' ';
 $noturno = $funcionamento['turnos']['noturno'] == 'sim' ? 'Noturno' : ' ';
-
 require("../includes/main/main_visualizar_sala.php");
 require("../includes/footer/footer.php");
 ?>
