00be185908ac35af2485f96bdd0241b22775806c
Arthur Simões <arthur2006.teixeira@gmail.com>
2024-03-31 14:56:48 -0300

    order desc checklists concluidas

--- app/Entity/Checklist.php
+++ app/Entity/Checklist.php
@@ -39,10 +39,11 @@ class Checklist
 
     public static function getDoneCheck($inicio = 0) {
         $obj_banco = new Banco('check_concluidas');
-        $data = [$obj_banco -> select(limit: "$inicio, 5") -> fetchAll(PDO::FETCH_ASSOC), $obj_banco -> select(campos: 'count(*) as total') -> fetch(PDO::FETCH_ASSOC)];
+        $data = [$obj_banco -> select(limit: "$inicio, 5",order:'data_fechamento DESC') -> fetchAll(PDO::FETCH_ASSOC), $obj_banco -> select(campos: 'count(*) as total') -> fetch(PDO::FETCH_ASSOC)];
+        // print_r($obj_banco -> select(limit: "$inicio, 5") -> fetchAll(PDO::FETCH_ASSOC));
         return $data;
     }
-    public static function getChecklist()
+        public static function getChecklist()
     {
         $obj_banco = new Banco('cadastro_checklist');
         $dados = $obj_banco->select(order:"id DESC");
